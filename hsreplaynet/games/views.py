33ce595b3ab7e74f6468d4b3ddb0f8ac086172f9
Jerome Leclanche <jerome@leclan.ch>
2016-10-27 15:53:17 +0300

    Revamp ReplayDetailView to get playlists from querystring

--- hsreplaynet/games/views.py
+++ hsreplaynet/games/views.py
@@ -3,7 +3,7 @@ from django.http import Http404
 from django.shortcuts import render
 from django.views.generic import View
 from django.views.decorators.clickjacking import xframe_options_exempt
-from .models import GameReplay
+from .models import GameReplay, PlayList
 
 
 class MyReplaysView(LoginRequiredMixin, View):
@@ -14,26 +14,49 @@ class MyReplaysView(LoginRequiredMixin, View):
 
 
 class ReplayDetailView(View):
-	def get(self, request, id):
-		replay = GameReplay.objects.find_by_short_id(id)
-		if not replay:
-			raise Http404("Replay not found")
+	template_name = "games/replay_detail.html"
 
-		# TODO: IP caching in redis
-		replay.views += 1
-		replay.save()
-
+	def get_context(self, request, shortid):
+		replay = self.get_replay(shortid)
+		playlist = self.get_playlist(request.GET.get("playlist", ""))
 		players = replay.global_game.players.all()
 		players = players.prefetch_related("deck_list", "deck_list__includes")
-
 		baseurl = "%s://%s" % (request.scheme, request.get_host())
-		return render(request, "games/replay_detail.html", {
+
+		return {
 			"replay": replay,
+			"playlist": playlist,
 			"title": replay.pretty_name_spoilerfree,
 			"canonical_url": baseurl + replay.get_absolute_url(),
 			"players": players,
-			"twitter_card": request.GET.get("twitter_card", "summary")
-		})
+			"twitter_card": request.GET.get("twitter_card", "summary"),
+		}
+
+	def get_playlist(self, shortid):
+		if not shortid:
+			return
+		try:
+			playlist = PlayList.objects.get(shortid=shortid)
+		except PlayList.DoesNotExist:
+			# We don't want to 404 if the playlist does not exist
+			# The replay could still be valid
+			playlist = None
+		return playlist
+
+	def get_replay(self, shortid):
+		replay = GameReplay.objects.find_by_short_id(shortid)
+		if not replay:
+			raise Http404("Replay not found")
+
+		# Increase view counter on replay
+		# TODO: IP caching in redis
+		replay.views += 1
+		replay.save()
+		return replay
+
+	def get(self, request, id):
+		context = self.get_context(request, id)
+		return render(request, self.template_name, context)
 
 
 class ReplayEmbedView(View):
