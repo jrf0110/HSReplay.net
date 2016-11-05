597292ca305add6fc75409ddf65af0407c454580
Benedict Etzel <developer@beheh.de>
2016-11-05 00:25:25 -0700

    Rename featureguard template tag feature

--- hsreplaynet/features/templatetags/features.py
+++ hsreplaynet/features/templatetags/features.py
@@ -6,28 +6,15 @@ from hsreplaynet.utils.instrumentation import error_handler
 register = template.Library()
 
 
-@register.simple_tag(takes_context=True)
-def feature(context, feature_name):
-	"""
-	Expected usage is:
-
-	{% feature "winrates" as winrates %}
-	{% if winrates.is_enabled %}
-		...
-		{% if winrates.read_only %} ... {% endif %}
-		...
-	{% endif %}
-	"""
+def get_feature_context(user, feature_name):
 	feature_context = {
 		"is_enabled": True,
-		"read_only": False
+		"read_only": False,
 	}
 
 	if settings.DEBUG:
 		# Feature policies are not enforced in development mode
 		return feature_context
-
-	user = context["request"].user
 
 	try:
 		feature = Feature.objects.get(name=feature_name)
@@ -44,36 +31,61 @@ def feature(context, feature_name):
 
 
 @register.tag
-def featureguard(parser, token):
+def feature(parser, token):
 	"""
 	Expected usage:
 
-	{% featureguard "winrate" %}
+	{% feature "winrate" %}
 		...
-	{% endfeatureguard %}
+	{% endfeature %}
+
+	or
+
+	{% feature "winrate" as winrate %}
+		...
+	{% endfeature %}
 	"""
-	try:
-		tag_name, feature_name = token.split_contents()
-	except ValueError:
+	contents = token.split_contents()
+	variable = None
+	if len(contents) == 2:
+		tag_name, feature_name = contents
+	elif len(contents) == 4:
+		tag_name, feature_name, as_token, variable = contents
+		if as_token != "as":
+			raise template.TemplateSyntaxError(
+				"%r tag's feature should only be followed directly by 'as' keyword" %
+				token.contents.split()[0]
+			)
+	else:
 		raise template.TemplateSyntaxError(
 			"%r tag requires exactly one argument" % token.contents.split()[0]
 		)
 	if not (feature_name[0] == feature_name[-1] and feature_name[0] in ('"', "'")):
 		raise template.TemplateSyntaxError(
-			"%r tag's argument should be in quotes" % tag_name
+			"%r tag's feature should be in quotes" % tag_name
 		)
-	nodelist = parser.parse(('end' + tag_name,))
+	if len(contents) == 4 and variable[0] in ('"', "'"):
+		raise template.TemplateSyntaxError(
+			"%r tag's variable should not be quoted" % tag_name
+		)
+	nodelist = parser.parse(('endfeature',))
 	parser.delete_first_token()
-	return GuardNode(feature_name[1:-1], nodelist)
+	return GuardNode(feature_name[1:-1], nodelist, variable)
 
 
 class GuardNode(template.Node):
-	def __init__(self, feature_name, nodelist):
+	def __init__(self, feature_name, nodelist, variable):
 		self.feature_name = feature_name
 		self.nodelist = nodelist
+		self.variable = variable
 
 	def render(self, context):
-		feature_context = feature(context, self.feature_name)
+		feature_context = get_feature_context(context["request"].user, self.feature_name)
 		if feature_context["is_enabled"]:
-			return self.nodelist.render(context)
+			if self.variable:
+				context[self.variable] = feature_context
+			result = self.nodelist.render(context)
+			if self.variable:
+				del context[self.variable]
+			return result
 		return ''
