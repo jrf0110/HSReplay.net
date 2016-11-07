840a105767ca57120a0ebac182d0026bd4d99cb2
Benedict Etzel <developer@beheh.de>
2016-11-06 16:35:57 -0800

    Improve matrix component

--- hsreplaynet/static/scripts/src/components/Matrix.tsx
+++ hsreplaynet/static/scripts/src/components/Matrix.tsx
@@ -14,9 +14,26 @@ interface NumberRow {
 }
 
 interface MatrixState {
+	intensity?: number;
+	colors?: Colors;
 }
 
+const enum Colors {
+	REDGREEN,
+	REDGREEN2,
+	ORANGEBLUE,
+};
+
 export default class Matrix extends React.Component<MatrixProps, MatrixState> {
+
+	constructor(props: MatrixProps, context: any) {
+		super(props, context);
+
+		this.state = {
+			intensity: 0.25,
+			colors: Colors.REDGREEN,
+		};
+	}
 
 	public render(): JSX.Element {
 		let rowcount = 0;
@@ -30,7 +47,7 @@ export default class Matrix extends React.Component<MatrixProps, MatrixState> {
 				const matchup: any = row[key];
 				const winrate = matchup ? (matchup.f_wr_vs_o).toFixed(2) + "%" : "unknown";
 				const style = {
-					backgroundColor: "hsl(" + (matchup ? Math.round(matchup.f_wr_vs_o) : 50) + ", 100%, 50%)",
+					backgroundColor: this.getColorString(matchup ? matchup.f_wr_vs_o : null, matchup ? !!matchup.is_mirror : false),
 				};
 				return <td key={cellcount++} title={class1 + " vs " + class2 + String.fromCharCode(10) + "Winrate: " + winrate} style={style}></td>;
 			});
@@ -47,10 +64,99 @@ export default class Matrix extends React.Component<MatrixProps, MatrixState> {
 		}));
 
 		return (
-			<table className="table table-condensed table-hover table-bordered" style={{fontSize: "0.7em", tableLayout: "fixed"}}>
-				{headers}
-				{rows}
-			</table>
+			<div className="component-matrix">
+				<table style={{fontSize: "0.7em"}}>
+					{headers}
+					{rows}
+				</table>
+				<div>
+					<label>
+						Intensity
+						<input type="range" min={0} max={100} value={"" + this.state.intensity * 100}
+							   onChange={(e: any) => {
+							this.setState({
+								intensity: +e.target.value / 100,
+							});
+						}} />
+					</label>
+					<label>
+						Colors
+						<select value={"" + this.state.colors} onChange={(e: any) => {
+							this.setState({
+								colors: +e.target.value,
+							});
+						}}>
+							<option value={"" + Colors.REDGREEN}>Red/Green</option>
+							<option value={"" + Colors.REDGREEN2}>Alternate Red/Green</option>
+							<option value={"" + Colors.ORANGEBLUE}>Orange/Blue</option>
+						</select>
+					</label>
+				</div>
+			</div>
 		);
+	}
+
+	private getColorString(winrate: number, mirror: boolean): string {
+		if (mirror) {
+			return "black";
+		}
+
+		if (winrate === null) {
+			return "gray";
+		}
+
+		let neutral = [0, 100, 100];
+		let positive = [0, 0, 0];
+		let negative = [0, 0, 0];
+
+		switch(this.state.colors) {
+			case Colors.REDGREEN:
+				positive = [120, 60, 50];
+				neutral = [60, 100, 100];
+				negative = [0, 100, 65.7];
+				break;
+			case Colors.REDGREEN2:
+				positive = [120, 60, 50];
+				neutral = [null, 100, 100];
+				negative = [0, 100, 65.7];
+				break;
+			case Colors.ORANGEBLUE:
+				positive = [202, 100, 50];
+				neutral = [null, 100, 100];
+				negative = [41, 100, 50];
+				break;
+		}
+
+		const _fn = (x: number, from: number, to: number): number => {
+			if(from === null || to === null) {
+				return +(to || from);
+			}
+			x = Math.pow(x, this.state.intensity);
+			return from + (to - from) * x;
+		};
+
+		const fn = (x: number, from: number[], to: number[]): number[] => {
+			return [
+				_fn(x, from[0], to[0]),
+				_fn(x, from[1], to[1]),
+				_fn(x, from[2], to[2]),
+			];
+		};
+
+		const hsl = (hsl: number[]): string => {
+			return "hsl(" + hsl[0] + ", " + hsl[1] + "%, " + hsl[2] + "%)";
+		};
+
+		const severity = Math.abs(50 - winrate) / 100 * 2;
+
+		if(winrate > 50) {
+			return hsl(fn(severity, neutral, positive));
+		}
+		else if(winrate < 50) {
+			return hsl(fn(severity, neutral, negative));
+		}
+		else {
+			return hsl(neutral);
+		}
 	}
 }
