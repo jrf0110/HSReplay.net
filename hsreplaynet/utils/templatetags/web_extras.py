ea686fd27e785a137a4cecb877afe4eaf5105063
Andrew Wilson <andrewmoorewilson@gmail.com>
2016-11-13 16:28:25 -0800

    Add template tag to render deck list meta tags based on query params

--- hsreplaynet/utils/templatetags/web_extras.py
+++ hsreplaynet/utils/templatetags/web_extras.py
@@ -2,6 +2,9 @@ from django import template
 from django.conf import settings
 from humanize import naturaldelta, naturaltime
 from datetime import datetime
+from hearthstone.enums import CardType
+from hsreplaynet.cards.models import Card, Deck
+from hsreplaynet.cards.archetypes import guess_class
 from hsreplaynet.games.models import GameReplay
 from re import match, IGNORECASE
 from django.contrib.staticfiles.templatetags.staticfiles import static
@@ -58,3 +61,28 @@ def static_absolute(context, value):
 	if not match("[a-z]([a-z0-9+-.])*:", value, IGNORECASE):
 		value = "%s://%s%s" % (request.scheme, request.get_host(), value)
 	return value
+
+
+@register.inclusion_tag("deck_list_meta_block.html", takes_context=True)
+def render_deck_list_meta_for_digest_param(context):
+	request = context.request
+	deck_digest = request.GET.get("deck_digest", "")
+	result = {
+		"render_meta_tag": False
+	}
+
+	if deck_digest:
+		deck = Deck.objects.filter(digest=deck_digest).first()
+		if deck:
+			class_guess = guess_class(deck)
+			if class_guess:
+				hero_card = Card.objects.filter(
+					card_class=class_guess,
+					type=CardType.HERO
+				).first()
+				if hero_card:
+					result["render_meta_tag"] = True
+					result["card_ids"] = ",".join(deck.card_id_list())
+					result["hero_id"] = str(hero_card.id)
+
+	return result
