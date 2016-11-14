5008c3be9669f6ad2f7f8e5e88e38957cbf35366
Benedict Etzel <developer@beheh.de>
2016-11-14 15:28:39 +0100

    Archetypes: Add RankRangeSelector component

--- hsreplaynet/static/scripts/src/components/ArchetypeClient.tsx
+++ hsreplaynet/static/scripts/src/components/ArchetypeClient.tsx
@@ -3,6 +3,7 @@ import Distribution from "./Distribution";
 import {BnetGameType} from "../hearthstone";
 import Matrix from "./stats/Matrix";
 import SampleSizeSelector from "./stats/controls/SampleSizeSelector";
+import RankRangeSelector from "./stats/controls/RankRangeSelector";
 
 interface ArchetypeClientProps extends React.ClassAttributes<ArchetypeClient> {
 }
@@ -11,14 +12,68 @@ interface ArchetypeClientState {
 	popularities?: any;
 	winrates?: any;
 	sampleSize?: number;
+	smallestRank?: number;
+	largestRank?: number;
 }
 
 export default class ArchetypeClient extends React.Component<ArchetypeClientProps, ArchetypeClientState> {
 
 	constructor(props: ArchetypeClientProps, context: any) {
 		super(props, context);
+		this.state = {
+			popularities: {},
+			winrates: {},
+			sampleSize: 0,
+			smallestRank: 0,
+			largestRank: 25,
+		};
+		this.fetch();
+	}
+
+	public render(): JSX.Element {
+		return <div>
+			<div className="row">
+				<div className="col-lg-3">
+					<RankRangeSelector
+						smallest={this.state.smallestRank}
+						onChangeSmallest={(smallestRank: number): void => {
+								this.setState({smallestRank: smallestRank});
+							}}
+						largest={this.state.largestRank}
+						onChangeLargest={(largestRank: number): void => {
+								this.setState({largestRank: largestRank});
+							}}
+					/>
+					<SampleSizeSelector
+						sampleSize={this.state.sampleSize}
+						onChangeSampleSize={(sampelSize: number): void => this.setState({sampleSize: sampelSize})}
+					/>
+				</div>
+				<div className="col-lg-9">
+					<Matrix
+						matrix={this.state.winrates}
+						sampleSize={this.state.sampleSize}
+					/>
+				</div>
+			</div>
+			<h2>Popularities</h2>
+			<Distribution
+				distributions={this.state.popularities}
+				title="Archetype"
+				value="Popularity"
+			/>
+		</div>;
+	}
+
+	public componentDidUpdate(prevProps: ArchetypeClientProps, prevState: ArchetypeClientState, prevContext: any): void {
+		if (prevState.smallestRank !== this.state.smallestRank || prevState.largestRank !== this.state.largestRank) {
+			this.fetch();
+		}
+	}
+
+	private fetch(): void {
 		fetch(
-			"/cards/winrates/?lookback=7&game_types=" + BnetGameType.BGT_RANKED_STANDARD + "," + BnetGameType.BGT_CASUAL_STANDARD + "&min_rank=0&max_rank=25",
+			"/cards/winrates/?lookback=7&game_types=" + BnetGameType.BGT_RANKED_STANDARD + "," + BnetGameType.BGT_CASUAL_STANDARD + "&min_rank=" + this.state.smallestRank + "&max_rank=" + this.state.largestRank,
 			{
 				credentials: "include",
 			}
@@ -30,36 +85,5 @@ export default class ArchetypeClient extends React.Component<ArchetypeClientProp
 				winrates: json.win_rates,
 			});
 		});
-		this.state = {
-			popularities: {},
-			winrates: {},
-			sampleSize: 0,
-		};
-	}
-
-	public render(): JSX.Element {
-		return (
-			<div className="row">
-				<div className="col-lg-9 col-lg-push-3">
-					<h2>Winrates</h2>
-					<Matrix
-						matrix={this.state.winrates}
-						sampleSize={this.state.sampleSize}
-					/>
-					<SampleSizeSelector
-						sampleSize={this.state.sampleSize}
-						onChangeSampleSize={(sampleSize: number): void => this.setState({sampleSize: sampleSize})}
-					/>
-				</div>
-				<div className="col-lg-3 col-lg-pull-9">
-					<h2>Popularities</h2>
-					<Distribution
-						distributions={this.state.popularities}
-						title="Archetype"
-						value="Popularity"
-					/>
-				</div>
-			</div>
-		);
 	}
 }
---
+++ hsreplaynet/static/scripts/src/components/stats/controls/RankRangeSelector.tsx
@@ -0,0 +1,137 @@
+import * as React from "react";
+
+interface RankRangeSelectorProps extends React.ClassAttributes<RankRangeSelector> {
+	smallest?: number;
+	onChangeSmallest?: (smallest: number) => void;
+	largest?: number;
+	onChangeLargest?: (largest: number) => void;
+	disabled?: boolean;
+}
+
+interface RankRangeSelectorState {
+	smallest?: number;
+	largest?: number;
+	instantCommit?: boolean;
+}
+
+/**
+ * Allows the selection of a rank range, from a smallest rank or legend
+ *
+ */
+export default class RankRangeSelector extends React.Component<RankRangeSelectorProps, RankRangeSelectorState> {
+
+	constructor(props: RankRangeSelectorProps, context: any) {
+		super(props, context);
+		this.state = {
+			smallest: null,
+			largest: null,
+			instantCommit: true,
+		}
+	}
+
+	public render(): JSX.Element {
+		const smallest = this.state.smallest !== null ? this.state.smallest : this.props.smallest;
+		const largest = this.state.largest !== null ? this.state.largest : this.props.largest;
+
+		return <div>
+			<label>
+				From
+				<input
+					type="number"
+					value={smallest === 0 ? "" : ""+smallest}
+					placeholder={smallest === 0 ? "Legend" : "Rank"}
+					disabled={!this.canChangeSmallest}
+					onChange={(e: any): void => {
+						if(!this.canChangeSmallest) {
+							return;
+						}
+						let smallest = Math.min(Math.max(+e.target.value, 0), 25);
+						if(smallest > largest && !this.canChangeLargest) {
+							// if we can't change largest, clamp to it
+							smallest = largest;
+						}
+						this.setState({
+							smallest: smallest,
+						});
+					}}
+					onFocus={() => this.prepare()}
+					onBlur={() => this.commit()}
+				/>
+			</label>
+			<label>
+				To
+				<input
+					type="number"
+					value={largest === 0 ? "" : "" + largest}
+					placeholder={largest === 0 ? "Legend" : "Rank"}
+					disabled={!this.canChangeLargest}
+					onChange={(e: any): void => {
+						if(!this.canChangeLargest) {
+							return;
+						}
+						let largest = Math.min(Math.max(+e.target.value, 0), 25);
+						if(largest < smallest && !this.canChangeSmallest) {
+							// if we can't change smallest, clamp to it
+							largest = smallest;
+						}
+						this.setState({
+							largest: largest,
+						});
+					}}
+					onFocus={() => this.prepare()}
+					onBlur={() => this.commit()}
+				/>
+			</label>
+		</div>;
+	}
+
+	protected get canChangeSmallest(): boolean {
+		return !!this.props.onChangeSmallest && !this.props.disabled;
+	}
+
+	protected get canChangeLargest(): boolean {
+		return !!this.props.onChangeLargest && !this.props.disabled;
+	}
+
+	protected prepare(): void {
+		this.setState({
+			instantCommit: false,
+		});
+	}
+
+	protected rollback(): void {
+		this.setState({
+			smallest: this.props.smallest,
+			largest: this.props.largest,
+		});
+	}
+
+	protected commit(): void {
+		let smallest = this.state.smallest;
+		let largest = this.state.largest;
+
+		if (largest !== null && largest < this.props.smallest) {
+			smallest = largest;
+		}
+
+		if (smallest !== null && smallest !== this.props.smallest && this.canChangeSmallest) {
+			this.props.onChangeSmallest(smallest);
+		}
+
+		if (smallest !== null && smallest > this.props.largest) {
+			// we're trying to change smallest, but need to shift largest
+			largest = smallest;
+		}
+
+		if (largest !== null && largest !== this.props.largest && this.canChangeLargest) {
+			this.props.onChangeLargest(largest);
+		}
+
+		this.setState({
+			smallest: null,
+			largest: null,
+			instantCommit: true,
+		})
+	}
+
+}
