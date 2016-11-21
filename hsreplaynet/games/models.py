0b5d95093a323ee4aaf0aa4725c0a7bbf36f679b
Andrew Wilson <andrewmoorewilson@gmail.com>
2016-11-21 17:20:45 -0500

    Remove two duplicate indexes on global_games digest field

--- hsreplaynet/games/models.py
+++ hsreplaynet/games/models.py
@@ -89,7 +89,7 @@ class GlobalGame(models.Model):
 	num_entities = models.IntegerField(null=True, blank=True)
 
 	digest = models.CharField(
-		max_length=40, unique=True, null=True, db_index=True,
+		max_length=40, unique=True, null=True,
 		help_text="SHA1 of str(game_handle), str(server_address), str(lo1), str(lo2)"
 	)
 	# has_private_replays = models.BooleanField(
