2acafbd92995d30702eec67182946922b340f36b
Jerome Leclanche <jerome@leclan.ch>
2016-11-21 16:45:09 +0200

    Add a command to merge users
    Closes #59

---
+++ hsreplaynet/accounts/management/commands/merge_users.py
@@ -0,0 +1,27 @@
+from django.core.management.base import BaseCommand, CommandError
+from ...models import User
+from ...utils import merge_users
+
+
+class Command(BaseCommand):
+	def add_arguments(self, parser):
+		parser.add_argument("base_user", nargs=1)
+		parser.add_argument("user", nargs=1)
+
+	def handle(self, *args, **options):
+		base_username = options["base_user"]
+		username = options["user"]
+
+		try:
+			base_user = User.objects.get(username=base_username[0])
+			user = User.objects.get(username=username[0])
+		except User.DoesNotExist as e:
+			raise CommandError(e)
+
+		if input("Merge user %r into %r? [y/N] " % (user, base_user)).lower() != "y":
+			raise CommandError("Not merging users.")
+			return
+
+		merge_users(base_user, user)
+
+		self.stdout.write("Users have been merged.")
---
+++ hsreplaynet/accounts/utils.py
@@ -0,0 +1,32 @@
+from hsreplaynet.utils import log
+
+
+def merge_users(base_user, user):
+	"""
+	Merge \a user into \a base_user
+	"""
+	def do_queryset(qs, **kwargs):
+		if not kwargs:
+			kwargs = {"user": base_user}
+		ret = qs.update(**kwargs)
+		log.info("Merging %r -> %r: %r", user, base_user, ret)
+		return ret
+
+	# Auth tokens
+	do_queryset(user.auth_tokens)
+
+	# Replays
+	do_queryset(user.replays)
+
+	# Comments
+	do_queryset(user.comment_comments)
+	do_queryset(user.comment_flags)
+
+	# Emails
+	do_queryset(user.emailaddress_set)
+
+	# Social accounts
+	do_queryset(user.socialaccount_set)
+
+	# Webhooks
+	do_queryset(user.webhooks)
