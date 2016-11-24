c757443ee5774b41002de0b72016617a58fddad8
Jerome Leclanche <jerome@leclan.ch>
2016-11-24 20:31:07 +0200

    Add a --zipfile argument to manage.py list_games to create a zip

--- hsreplaynet/games/management/commands/list_games.py
+++ hsreplaynet/games/management/commands/list_games.py
@@ -1,3 +1,6 @@
+import os.path
+import zipfile
+from django.core.files.storage import default_storage
 from django.core.management.base import BaseCommand
 from ...models import GameReplay
 
@@ -20,6 +23,10 @@ class Command(BaseCommand):
 			"--uploads", action="store_true",
 			help="List upload Power.log instead of hsreplay.xml file"
 		)
+		parser.add_argument(
+			"--zipfile", nargs="?", type=str,
+			help="Write all filtered games to this zip"
+		)
 
 	def handle(self, *args, **options):
 		games = GameReplay.objects.all()
@@ -39,6 +46,38 @@ class Command(BaseCommand):
 			results = games.values_list("uploads__file")
 		else:
 			results = games.values_list("replay_xml")
+		results = [path for (path, ) in results]
 
-		for (path, ) in results:
-			print(path)
+		zip_name = options["zipfile"]
+		if zip_name:
+			self.make_zip(zip_name, results)
+		else:
+			for path in results:
+				self.stdout.write(path)
+
+	def make_zip(self, zipname, results):
+		written = 0
+		failed = []
+
+		with open(zipname, "wb") as final_zip:
+			with zipfile.PyZipFile(final_zip, "w", zipfile.ZIP_DEFLATED) as zf:
+				for path in results:
+					basename = os.path.basename(path)
+
+					try:
+						f = default_storage.open(path)
+					except Exception as e:
+						self.stderr.write("Cannot open %r: %s" % (path, e))
+						failed.append(path)
+						continue
+
+					zf.writestr(basename, f.read())
+					self.stdout.write(path)
+					written += 1
+
+		self.stdout.write("Written %i objects to %r" % (written, final_zip.name))
+		if failed:
+			self.stderr.write("The following files failed to open:")
+			for path in failed:
+				self.stderr.write("  %s" % (path))
+			exit(8)
