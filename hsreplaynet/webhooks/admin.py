5c16c3fdfd37fe8c3e7e1ed1ec2c455a7d5ef42c
Jerome Leclanche <jerome@leclan.ch>
2016-11-24 04:37:25 +0200

    Add an admin action to send a test payload to a webhook

--- hsreplaynet/webhooks/admin.py
+++ hsreplaynet/webhooks/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from .models import Webhook, WebhookTrigger
 
 
+def send_test_payload(admin, request, queryset):
+	for obj in queryset:
+		obj.trigger({"test_data": True})
+send_test_payload.short_description = "Send test payload"
+
+
 @admin.register(Webhook)
 class WebhookAdmin(admin.ModelAdmin):
 	list_display = (
@@ -10,6 +16,7 @@ class WebhookAdmin(admin.ModelAdmin):
 	list_filter = ("is_active", "is_deleted")
 	raw_id_fields = ("user", )
 	search_fields = ("url", "user", )
+	actions = (send_test_payload, )
 
 
 @admin.register(WebhookTrigger)
