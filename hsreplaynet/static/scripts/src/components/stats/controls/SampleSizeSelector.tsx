cb108197cb6bafaf5ad6ed93e8f600dd1e139714
Benedict Etzel <benedict@hearthsim.net>
2016-11-24 22:28:23 +0100

    Sample Size Selector: Remove buttons (for now)

--- hsreplaynet/static/scripts/src/components/stats/controls/SampleSizeSelector.tsx
+++ hsreplaynet/static/scripts/src/components/stats/controls/SampleSizeSelector.tsx
@@ -11,47 +11,18 @@ export default class SampleSizeSelector extends React.Component<SampleSizeSelect
 		return <div className="control-sample-size-selector">
 			<label className="control-label">
 				<span>Sample Size</span>
-				<div className="input-group">
-					<input
-						type="number"
-						className="form-control"
-						value={"" + this.props.sampleSize}
-						disabled={!this.canChange}
-						onChange={(e: any): void => {
-							if(!this.canChange) {
-								return;
-							}
-							this.props.onChangeSampleSize(+e.target.value);
-						}}
-					/>
-					<span className="input-group-btn">
-						<button
-							className="btn btn-default"
-							disabled={!this.canChange}
-							onClick={() => this.sampleSize = 0}
-						>
-							Any
-						</button>
-					</span>
-					<span className="input-group-btn hidden-md hidden-lg">
-						<button
-							className="btn btn-default"
-							disabled={!this.canChange}
-							onClick={() => this.sampleSize = 50}
-						>
-							50
-						</button>
-					</span>
-					<span className="input-group-btn hidden-md">
-						<button
-							className="btn btn-default"
-							disabled={!this.canChange}
-							onClick={() => this.sampleSize = 100}
-						>
-							100
-						</button>
-					</span>
-				</div>
+				<input
+					type="number"
+					className="form-control"
+					value={"" + this.props.sampleSize}
+					disabled={!this.canChange}
+					onChange={(e: any): void => {
+						if(!this.canChange) {
+							return;
+						}
+						this.props.onChangeSampleSize(+e.target.value);
+					}}
+				/>
 			</label>
 		</div>;
 	}
