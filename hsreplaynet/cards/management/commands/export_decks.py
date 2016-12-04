0c452d44e68daa752773cb1922baec03ff3ad960
Andrew Wilson <andrewmoorewilson@gmail.com>
2016-12-03 22:04:22 -0500

    Improved deck exporting for archetype classification training

--- hsreplaynet/cards/management/commands/export_decks.py
+++ hsreplaynet/cards/management/commands/export_decks.py
@@ -1,45 +1,97 @@
+import json
+from datetime import date, timedelta
 from django.core.management.base import BaseCommand
 from django.db import connection
-from hearthstone.enums import CardClass
+from hsreplaynet.cards.models import Deck
+from hsreplaynet.cards.archetypes import guess_class
+from hsreplaynet.utils.db import execute_query
 
-QUERY = """
-SELECT
-	c.card_class AS "player_class", (
-		SELECT string_agg(
-			CASE WHEN ci.count > 1 THEN
-				left(repeat(concat(ci.card_id, ', '), ci.count), -2)
-			ELSE
-				ci.card_id END, ', '
-			) AS "deck_list"
-		FROM cards_deck cd
-		JOIN cards_include ci ON ci.deck_id = cd.id
-		WHERE cd.id = gp.deck_list_id
-	) AS "deck_list"
-FROM games_globalgameplayer gp
-JOIN games_globalgame gg ON gp.game_id = gg.id
-JOIN card c ON gp.hero_id = c.id
-WHERE gp.is_ai = FALSE
-AND gg.match_start > now()- INTERVAL '%s hour'
+
+QUERY_DECK_FEATURE_VEC = """
+SELECT array_agg(t.id) AS "vector" FROM (
+SELECT c.id::text AS "id"
+FROM card c
+WHERE c.collectible = TRUE AND c.type != 3
+ORDER BY c.id
+) t;
+"""
+
+
+DECKS_QUERY = """
+SELECT dss.deck_id, sum(dss.matches) AS "observations"
+FROM deck_summary_stats dss
+WHERE dss.epoch_seconds BETWEEN date_part('epoch', DATE '%s') AND date_part(
+'epoch', DATE '%s')
+AND dss.game_type = 2
+GROUP BY dss.deck_id
+HAVING sum(dss.matches) >= 50;
 """
 
 
 class Command(BaseCommand):
 	def add_arguments(self, parser):
 		parser.add_argument(
-			"--lookback", default=1, type=int,
-			help="The number of hours back from which we start the export"
+			"--lookback", default="1",
+			help="How many days back we look from the present"
 		)
 		parser.add_argument(
-			"--out", default="decks.csv",
-			help="The file where we will write the decks"
+			"--offset", default="0",
+			help="How many days back we shift the time window"
+		)
+		parser.add_argument(
+			"--out", default="decks.json",
+			help="The file where we will write the output too"
 		)
 
-	def handle(self, *args, **options):
+	def generate_card_id_maps(self):
 		cursor = connection.cursor()
-		lookback = options["lookback"]
-		cursor.execute(QUERY % lookback)
+		cursor.execute(QUERY_DECK_FEATURE_VEC)
+		results = list(cursor.fetchall())
+		map = {}
+		reverse_map = {}
+		for idx, card_id in enumerate(results[0][0]):
+			map[idx] = card_id
+			reverse_map[card_id] = idx
+		return map, reverse_map
 
-		with open(options["out"], mode="wt") as out:
-			for row in cursor.fetchall():
-				record = "%s:%s\n" % (CardClass(row[0]).name, row[1])
-				out.write(record)
+	def generate_deck_feature_vector(self, reverse_map, includes):
+		try:
+			return {reverse_map[id]: count for id, count in includes}
+		except:
+			return None
+
+	def handle(self, *args, **options):
+		result = {
+			"map": {},
+			"decks": {}
+		}
+
+		card_id_map, reverse_map = self.generate_card_id_maps()
+		result["map"] = card_id_map
+
+		offset = int(options["offset"])
+		lookback = int(options["lookback"])
+		today = date.today()
+		date_from = (today - timedelta(days=(offset + lookback))).isoformat()
+		date_to = (today - timedelta(days=offset)).isoformat()
+
+		for row in execute_query(DECKS_QUERY % (date_from, date_to)):
+			d = Deck.objects.get(id=row["deck_id"])
+
+			if len(d.card_id_list()) == 30:
+				player_class = guess_class(d)
+
+				if player_class and (player_class.value not in result["decks"]):
+					result["decks"][player_class.value] = []
+
+				includes = d.includes.values_list("card__id", "count")
+				deck_vector = self.generate_deck_feature_vector(reverse_map, includes)
+				if deck_vector and player_class:
+					deck_payload = {
+						"observations": row["observations"],
+						"cards": deck_vector
+					}
+					result["decks"][player_class.value].append(deck_payload)
+
+		with open(options["out"], "wt") as out:
+			out.write(json.dumps(result, indent=4))
