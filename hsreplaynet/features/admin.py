4b832fd0667c3689b4de015bc8b4afb6bcdbc3a6
Benedict Etzel <benedict@hearthsim.net>
2016-12-07 19:49:48 +0100

    Improve Feature admin

--- hsreplaynet/features/admin.py
+++ hsreplaynet/features/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Feature
 
-admin.site.register(Feature)
+
+@admin.register(Feature)
+class FeatureAdmin(admin.ModelAdmin):
+	list_display = ("name", "status", "read_only")
+	list_filter = ("status", "read_only", )
