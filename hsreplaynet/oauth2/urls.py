f7b57fa35eba461a10cdb637dd92d940ee6db59f
Jerome Leclanche <jerome@leclan.ch>
2016-12-08 11:43:19 +0200

    Add a view to revoke all tokens

--- hsreplaynet/oauth2/urls.py
+++ hsreplaynet/oauth2/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from oauth2_provider import views as oauth2_views
-from .views import ApplicationUpdateView, ApplicationListView
+from .views import ApplicationUpdateView, ApplicationListView, RevokeAllTokensView
 
 
 app_list_view = ApplicationListView.as_view()
@@ -10,5 +10,9 @@ authorization_view = oauth2_views.AuthorizationView.as_view()
 urlpatterns = [
 	url(r"^applications/$", app_list_view, name="oauth2_app_list"),
 	url(r"^application/(?P<pk>\d+)/$", app_update_view, name="oauth2_app_update"),
+	url(
+		r"^application/(?P<pk>\d+)/revoke_all_tokens/$",
+		RevokeAllTokensView.as_view(), name="oauth2_revoke_all_tokens"
+	),
 	url(r"^authorize/$", authorization_view, name="authorize"),
 ]
--- hsreplaynet/oauth2/views.py
+++ hsreplaynet/oauth2/views.py
@@ -1,18 +1,27 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import UpdateView, ListView
+from django.shortcuts import get_object_or_404, redirect
+from django.views.generic import UpdateView, ListView, View
 from oauth2_provider.models import Application
 
 
-class ApplicationUpdateView(LoginRequiredMixin, UpdateView):
+class ApplicationBaseView(LoginRequiredMixin, View):
 	model = Application
+
+	def get_queryset(self):
+		return self.model.objects.filter(user=self.request.user)
+
+
+class ApplicationUpdateView(ApplicationBaseView, UpdateView):
 	template_name = "oauth2/application_update.html"
 	fields = ("name", "redirect_uris")
 
 
-class ApplicationListView(LoginRequiredMixin, ListView):
-	model = Application
+class ApplicationListView(ApplicationBaseView, ListView):
 	template_name = "oauth2/application_list.html"
 
-	def get_queryset(self):
-		qs = super().get_queryset()
-		return qs.filter(user=self.request.user)
+
+class RevokeAllTokensView(ApplicationBaseView, View):
+	def post(self, request, **kwargs):
+		app = get_object_or_404(self.get_queryset(), pk=kwargs["pk"])
+		app.accesstoken_set.all().delete()
+		return redirect(app)
