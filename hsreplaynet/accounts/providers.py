58f79e746b0b74a788aa057f40ca488b29ad8ac4
Jerome Leclanche <jerome@leclan.ch>
2016-12-10 20:49:05 +0200

    Change BattleNetAdapter to drop dependency on upstream adapter
    The upstream adapter is getting removed.
    
    References pennersr/django-allauth#1556

--- hsreplaynet/accounts/providers.py
+++ hsreplaynet/accounts/providers.py
@@ -1,9 +1,19 @@
-from allauth.socialaccount.providers.battlenet.provider import BattleNetSocialAccountAdapter
+from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 
 
-class BattleNetAdapter(BattleNetSocialAccountAdapter):
+class BattleNetAdapter(DefaultSocialAccountAdapter):
 	def is_auto_signup_allowed(self, request, sociallogin):
 		battletag = sociallogin.account.extra_data.get("battletag")
 		if not battletag:
 			return False
 		return super().is_auto_signup_allowed(request, sociallogin)
+
+	def save_user(self, request, sociallogin, form=None):
+		user = super().save_user(request, sociallogin, form)
+		# XXX: Remove once we upgrade to Allauth 0.30
+		# See https://github.com/pennersr/django-allauth/pull/1556
+		battletag = sociallogin.account.extra_data.get("battletag")
+		if battletag:
+			user.username = battletag
+			user.save()
+		return user
