67bdb3c64444ab4464a38f1a4c33d6293a947366
Alexander Zeier <alex@hearthsim.net>
2016-12-15 21:38:21 +0100

    InfoBoxSection: add optional header style

--- hsreplaynet/static/scripts/src/components/InfoBoxSection.tsx
+++ hsreplaynet/static/scripts/src/components/InfoBoxSection.tsx
@@ -4,10 +4,12 @@ interface InfoBoxSectionState {
 	collapsed?: boolean;
 }
 
+export type HeaderStyle = "h1" | "h2";
 export type Size = "xs" | "sm" | "md" | "lg";
 
 export interface InfoBoxSectionProps extends React.ClassAttributes<InfoBoxSection> {
 	header: string;
+	headerStyle?: HeaderStyle;
 	collapsedSizes?: Size[];
 }
 
@@ -22,15 +24,20 @@ export default class InfoBoxSection extends React.Component<InfoBoxSectionProps,
 		const collapsable = this.props.collapsedSizes && !!this.props.collapsedSizes.length;
 		const contentClassNames = collapsable && this.state.collapsed ? this.props.collapsedSizes.map(x => "hidden-" + x).join(" ") : "";
 		const buttonClassNames = collapsable && this.props.collapsedSizes.map(x => "visible-" + x).join(" ");
+
+		const header = React.createElement(
+			this.props.headerStyle ? this.props.headerStyle : "h2",
+			null,
+			this.props.header,
+			collapsable ?
+				<a href="#" div className={"infobox-value " + buttonClassNames} onClick={() => this.setState({collapsed: !this.state.collapsed})}>
+					{this.state.collapsed ? "show" : "hide"}
+				</a> : null
+		)
+
 		return (
 			<div>
-				<h2>
-					{this.props.header}
-					{collapsable ?
-						<a href="#" div className={"infobox-value " + buttonClassNames} onClick={() => this.setState({collapsed: !this.state.collapsed})}>
-							{this.state.collapsed ? "show" : "hide"}
-						</a> : null }
-				</h2>
+				{header}
 				<div className={contentClassNames}>
 					{this.props.children}
 				</div>
