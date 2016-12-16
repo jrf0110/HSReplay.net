5c285491e2abdc2c11b2035b2842bb6835ad2361
Benedict Etzel <benedict@hearthsim.net>
2016-12-16 10:03:32 +0100

    Switch to Moment.js for timestamp formatting

--- hsreplaynet/static/scripts/src/PrettyTime.ts
+++ hsreplaynet/static/scripts/src/PrettyTime.ts
@@ -1,24 +1,9 @@
-
-function humanTime(seconds) {
-	// TODO: use something better
-	var days = Math.floor((seconds % 31536000) / 86400);
-	var hours = Math.floor(((seconds % 31536000) % 86400) / 3600);
-	var mins = Math.floor((((seconds % 31536000) % 86400) % 3600) / 60);
-	if (days) {
-		return days + " days";
-	}
-	if (hours) {
-		return hours + " hours";
-	}
-	return mins + " minutes";
-}
+import moment from "moment";
 
 export function getDuration(from: Date, to: Date): string {
-	var seconds = to.getTime() - from.getTime();
-	return humanTime(seconds / 1000);
+	return moment(from).from(moment(to), true);
 }
 
 export function getAge(since: Date): string {
-	var seconds = new Date().getTime() - since.getTime();
-	return humanTime(seconds / 1000) + " ago";
+	return moment(since).fromNow();
 }
