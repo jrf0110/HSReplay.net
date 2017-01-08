82e435be39a3d5b9e36fdf038d2f67f5eefee29e
Jerome Leclanche <jerome@leclan.ch>
2017-01-08 05:49:14 +0200

    articles: Add an atom feed for latest articles

---
+++ hsreplaynet/articles/feed.py
@@ -0,0 +1,28 @@
+from django.contrib.syndication.views import Feed
+from django.urls import reverse_lazy
+from django.utils.feedgenerator import Atom1Feed
+from markdownx.utils import markdownify
+from .models import Article
+
+
+class LatestArticlesFeed(Feed):
+	title = "HSReplay.net Articles"
+	link = reverse_lazy("articles_article_list")
+	description = "The latest Hearthstone articles from HSReplay.net"
+	feed_type = Atom1Feed
+
+	def items(self):
+		return Article.objects.order_by("-published", "-created")[:10]
+
+	def item_title(self, item):
+		return str(item)
+
+	def item_description(self, item):
+		return markdownify(item.contents)
+
+	def item_pubdate(self, item):
+		return item.pubdate
+
+	def author_name(self, item):
+		if item:
+			return str(item.author)
--- hsreplaynet/articles/urls.py
+++ hsreplaynet/articles/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from .feed import LatestArticlesFeed
 from . import views
 
 
@@ -8,5 +9,6 @@ list_view = views.ArticleListView.as_view()
 
 urlpatterns = [
 	url("^$", list_view, name="articles_article_list"),
+	url("^latest.atom$", LatestArticlesFeed(), name="articles_article_feed"),
 	url("^(?P<pk>\d+)/(?P<slug>\w+)?", detail_view, name="articles_article_detail"),
 ]
