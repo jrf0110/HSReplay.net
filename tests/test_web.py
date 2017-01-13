bb5739382dbe13c8a278a52ba94c59051fc7db7c
Jerome Leclanche <jerome@leclan.ch>
2017-01-13 03:37:29 +0200

    Add a very simple test to load the homepage

--- tests/test_web.py
+++ tests/test_web.py
@@ -5,3 +5,8 @@ def test_battlenet_auth_loaded():
 	assert registry.loaded
 	provider = registry.by_id("battlenet")
 	assert provider.id == "battlenet"
+
+
+def test_load_homepage(client):
+	response = client.get("/")
+	assert response.status_code == 200
