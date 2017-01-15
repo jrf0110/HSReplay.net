c39623941f3c110d94d913780c98e991f7f2c450
Jerome Leclanche <jerome@leclan.ch>
2017-01-15 10:50:03 +0200

    Include adsense only in articles, move to single footerjs template

---
+++ hsreplaynet/templates/footerjs.js
@@ -0,0 +1,31 @@
+{% load web_extras %}
+{% if request.dnt %}
+// You are sending DNT. We won't serve you Google Analytics or Ads.
+{% else %}
+{% setting "GOOGLE_ANALYTICS" as gua %}
+{% if gua %}
+(function(i, s, o, g, r, a, m) {
+	i["GoogleAnalyticsObject"] = r;
+	i[r] = i[r] || function() {
+		(i[r].q = i[r].q || []).push(arguments)
+	}, i[r].l = 1 * new Date();
+	a = s.createElement(o),
+	m = s.getElementsByTagName(o)[0];
+	a.async = 1;
+	a.src = g;
+	m.parentNode.insertBefore(a, m)
+})(window, document, "script", "https://www.google-analytics.com/analytics.js", "ga");
+ga("create", "{{ gua }}", "auto");
+ga("send", "pageview", {
+	page: location.pathname + location.search.replace(/((state)|(code))=\w+\&?/, "").replace(/^\?$/, "") + location.hash
+});
+{% endif %}
+
+{% setting "GOOGLE_ADSENSE" as adsense %}
+{% if adsense %}
+(adsbygoogle = window.adsbygoogle || []).push({
+	google_ad_client: "{{ adsense }}",
+});
+{% endif %}
+
+{% endif %}
