83bf4e1357c0315fb4efc2e50bc0536778eb3ddc
Andrew Wilson <andrewmoorewilson@gmail.com>
2017-01-16 19:36:34 +0100

    Add end point for invoking Redshift queries

--- hsreplaynet/analytics/views.py
+++ hsreplaynet/analytics/views.py
@@ -1,13 +1,36 @@
 import json
 from django.urls import reverse
+from django.conf import settings
 from hsredshift.analytics import queries
 from hsreplaynet.cards.models import Card
 from django.http import HttpResponse
+from sqlalchemy import create_engine
 
 
 # Create your views here.
 def run_query(request, name):
-	pass
+	conn_info = settings.REDSHIFT_CONNECTION
+	engine = create_engine(conn_info)
+	query = queries.get_query(name)
+	params = {}
+	for param_name, converter in query.params():
+		if param_name in request.GET:
+			params[param_name] = converter(request.GET[param_name])
+
+	results = query.as_result_set().execute(engine, params)
+
+	chart_series_data = query.to_chart_series(params, results)
+
+	result = {
+		"render_as": query.display_visual.name.lower(),
+		"labelX": query.label_x,
+		"labelY": query.label_y,
+		"title": query.title,
+		"series": chart_series_data
+	}
+
+	payload_str = json.dumps(result, indent=4, sort_keys=True)
+	return HttpResponse(payload_str, content_type="application/json")
 
 
 def card_inventory(request, card_id):
@@ -16,7 +39,7 @@ def card_inventory(request, card_id):
 	for query in queries.card_inventory(card):
 		query = {
 			"endpoint": reverse("analytics_run_query", kwargs={"name": query.name}),
-			"params": list(query.params())
+			"params": list(query.params().keys())
 		}
 		result.append(query)
 
