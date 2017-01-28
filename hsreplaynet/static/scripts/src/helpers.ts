11d74fac33291b8571d574dd7258d7fc15d972c0
Alexander Zeier <alex@hearthsim.net>
2017-01-28 18:53:37 +0100

    Implement ClassFilter component

--- hsreplaynet/static/scripts/src/helpers.ts
+++ hsreplaynet/static/scripts/src/helpers.ts
@@ -17,3 +17,26 @@ export function joustAsset(asset: string) {
 export function cardArt(cardArt: string) {
 	return HEARTHSTONE_ART_URL + cardArt + ".jpg";
 }
+
+export function toTitleCase(str: string) {
+	return str.substr(0, 1).toUpperCase() + str.substr(1, str.length - 1).toLowerCase();
+}
+
+export function getHeroColor(hero: string): string {
+	if (!hero) {
+		return;
+	}
+	switch (hero.toUpperCase()) {
+		case "DRUID": return "#FF7D0A";
+		case "HUNTER": return "#ABD473";
+		case "MAGE": return "#69CCF0";
+		case "PALADIN": return "#F58CBA";
+		case "PRIEST": return "#D2D2D2";
+		case "ROGUE": return "#FFF01a";
+		case "SHAMAN": return "#0070DE";
+		case "WARLOCK": return "#9482C9";
+		case "WARRIOR": return "#C79C6E";
+		case "ALL": return "#808080";
+		case "NEUTRAL": return "#808080";
+	}
+}
