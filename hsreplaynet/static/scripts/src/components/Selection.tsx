e94d2b298f7ed685f28a2ea3f07fb05b541502ad
Alexander Zeier <alex@hearthsim.net>
2017-01-28 17:17:37 +0100

    Improve selection component

--- hsreplaynet/static/scripts/src/components/Selection.tsx
+++ hsreplaynet/static/scripts/src/components/Selection.tsx
@@ -4,56 +4,109 @@ interface SelectionState {
 	selected?: string;
 }
 
+interface Option {
+	key: string;
+	name: string;
+	premium: boolean;
+}
+
 interface SelectionProps extends React.ClassAttributes<Selection> {
 	name: string;
-	visibleOptions: string[];
-	collapsedOptions?: string[];
-	defaultSelection?: string;
+	visible: Option[];
+	collapsed: Option[];
+	default?: string;
+	selectionChanged?: (string) => void;
+	premiumAvailable?: boolean;
 }
 
 export default class Selection extends React.Component<SelectionProps, SelectionState> {
 	constructor(props: SelectionProps, state: SelectionState) {
 		super(props, state);
 		this.state = {
-			selected: this.props.defaultSelection || this.props.visibleOptions[0],
+			selected: this.props.default || this.props.visible[0].key,
 		}
 	}
 
 	render(): JSX.Element {
 		const visible = [];
-		this.props.visibleOptions.forEach(name => {
-			const className = "btn btn-" + (name === this.state.selected ? "primary" : "default")
-			visible.push(<button type="button" className={className} onClick={() => this.onClick(name)}>{name}</button>);
+		const options = this.getSortedOptions();
+
+		options.visible.forEach(option => {
+			const selected = option.key === this.state.selected;
+			const className = "btn btn-" + (selected ? "primary" : "default") + this.disabled(option);
+			const width = 100/this.props.visible.length + "%";
+			visible.push(
+				<button data-toggle={selected ? "dropdown" : ""} style={{width: width}} type="button" className={className} onClick={() => this.onClick(option)}>
+					{option.name}{this.premiumGlyph(option)}
+				</button>
+			);
 		});
+
 		let dropDown = null;
-		if (this.props.collapsedOptions) {
+		if (options.collapsed) {
 			const items = [];
-			this.props.collapsedOptions && this.props.collapsedOptions.forEach(name => {
+			options.collapsed && options.collapsed.forEach(option => {
+				const cursor = !this.props.premiumAvailable && option.premium ? "not-allowed" : "pointer";
 				items.push(
-					<li>
-						<a href="#" style={{display: "inline"}} onClick={() => this.onClick(name)}>{name}</a>
-						<span className="glyphicon glyphicon-lock" style={{color: "black", float:"right"}}/>
+					<li style={{padding: 0, cursor: cursor}}>
+						<a className={"btn btn-default" + this.disabled(option)} href="#" style={{display: "block", padding: "0.5em 1em"}} onClick={() => this.onClick(option)}>
+							{option.name}
+							{this.premiumGlyph(option)}
+						</a>
 					</li>
 				);
 			})
-			const className = "btn btn-" + (this.props.collapsedOptions.indexOf(this.state.selected) === -1 ? "default" : "primary") + " dropdown-toggle";
 			dropDown = [
-				<button type="button" className={className} data-toggle="dropdown">
+				<button type="button" className="btn btn-default dropdown-toggle" data-toggle="dropdown">
 					<span className="caret"/>
 				</button>,
-				<ul className="dropdown-menu dropdown-menu-right">
+				<ul className="dropdown-menu dropdown-menu-right" style={{paddingBottom: 0}}>
 					{items}
 				</ul>
 			]
 		}
+
 		return <div className="input-group">
-			<span className="input-group-addon">{this.props.name}</span>
-			{visible}
-			{dropDown}
+			<div className="input-group-addon" style={{minWidth: "0px"}}>{}</div>
+			<div style={{display: "flex"}}>
+				{visible}
+				{dropDown}
+			</div>
 		</div>
 	}
 
-	onClick(name: string) {
-		this.setState({selected: name});
+	getSortedOptions(): {visible: Option[], collapsed: Option[]} {
+		// If a collapsed option is selected:
+		// - move option to place of last visible option
+		// - move old visible option to top collapsed options
+		const visibleOptions = this.props.visible.slice();
+		let collapsedOptions = this.props.collapsed.slice();
+		const collapsedSelected = this.props.collapsed.find(x => x.key == this.state.selected);
+		if (collapsedSelected) {
+			collapsedOptions.unshift(visibleOptions.pop());
+			visibleOptions.push(collapsedSelected);
+			collapsedOptions = collapsedOptions.filter(x => x.key !== collapsedSelected.key);
+		}
+		return {visible: visibleOptions, collapsed: collapsedOptions};
+	}
+
+	disabled(option: Option): string {
+		return !this.props.premiumAvailable && option.premium ? " disabled" : "";
+	}
+
+	onClick(option: Option) {
+		if (!option.premium || this.props.premiumAvailable) {
+			this.setState({selected: option.key});
+			if (this.props.selectionChanged) {
+				this.props.selectionChanged(option.key);
+			}
+		}
+	}
+
+	premiumGlyph(option: Option): JSX.Element {
+		if (!this.props.premiumAvailable && option.premium) {
+			return <span className="glyphicon glyphicon-lock disabled" style={{color: "gray", float:"right", marginTop: "3px"}}/>;
+		}
+		return null;
 	}
 }
