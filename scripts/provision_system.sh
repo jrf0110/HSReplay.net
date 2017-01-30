d7653bbc56011dce4b3a408dad4858d476bc6d03
Jerome Leclanche <jerome@leclan.ch>
2017-01-30 23:01:18 +0200

    vagrant: Replace nodeenv and npm with yarn

--- scripts/provision_system.sh
+++ scripts/provision_system.sh
@@ -10,8 +10,13 @@ echo 'deb http://cloudfront.debian.net/debian jessie-backports main
 deb-src http://cloudfront.debian.net/debian jessie-backports main' > /etc/apt/sources.list.d/backports.list
 echo "deb https://repos.influxdata.com/debian jessie stable" > /etc/apt/sources.list.d/influxdb.list
 echo "deb http://apt.postgresql.org/pub/repos/apt/ jessie-pgdg main" > /etc/apt/sources.list.d/postgres.list
+echo "deb https://deb.nodesource.com/node_7.x jessie main" > /etc/apt/sources.list.d/nodejs.list
+echo "deb https://dl.yarnpkg.com/debian/ stable main" > /etc/apt/sources.list.d/yarn.list
 wget https://repos.influxdata.com/influxdb.key -qO - | apt-key add -
 wget https://www.postgresql.org/media/keys/ACCC4CF8.asc -qO - | apt-key add -
+wget https://deb.nodesource.com/gpgkey/nodesource.gpg.key -qO - | apt-key add -
+wget https://dl.yarnpkg.com/debian/pubkey.gpg -qO - | apt-key add -
+
 
 apt-get update -q
 apt-get dist-upgrade -qy
@@ -19,6 +24,7 @@ apt-get install -qy \
 	zsh curl git htop tree unzip vim \
 	python3 python3-dev python3-venv \
 	gcc g++ libxml2 libxml2-dev libxslt1-dev \
+	nodejs yarn \
 	supervisor influxdb \
 	python-dev libffi-dev
 apt-get install -qy -t jessie-backports postgresql-9.6 libpq-dev
--- scripts/provision_user.sh
+++ scripts/provision_user.sh
@@ -14,29 +14,10 @@ source "$HOME/env/bin/activate"
 pip install --upgrade pip setuptools
 pip install -r "$PROJECT/requirements/dev.txt"
 
-nodeenv -p
 export PATH="$HOME/node_modules/.bin:$PATH"
 
-# Let's talk about awfulware.
-# There are so many layers at which this idiotic issue could have been prevented:
-# - If Microsoft didn't make symlinks suck on windows
-# - If Windows symlinks, sucky as they may be, were supported on Vagrant
-# - If Vagrant didn't sell itself as the solution to "it works on my machine"
-# - If those symlinks didn't exist in the first place
-# - If npm properly supported prefixing (npm/npm#775)
-# - If, more generally, npm wasn't one of the worst package managers ever created
-# - If we stopped producing a new package manager for every single bloody language
-# - If we as a community, and the JS community in particular, didn't let atrocious
-#   engineering decisions plague extremely popular software. Awfulware should not
-#   be allowed to become the norm.
-# ... but I suppose, if that were the case, Windows wouldn't have been so popular
-# in the first place and I wouldn't have to waste my time with this STUPID ISSUE.
-# https://github.com/npm/npm/issues/7308
-# https://github.com/npm/npm/issues/9901
-ln -fs "$PROJECT/package.json" "$HOME/package.json"
-# Move typings one level up
-sed 's|hsreplaynet/|hsreplay.net/hsreplaynet/|g' "$PROJECT/typings.json" > "$HOME/typings.json"
-npm install
+cd "$PROJECT"
+yarn install --modules-folder "$HOME"
 
 if [[ ! -e $PROJECT/hsreplaynet/local_settings.py ]]; then
 	cp "$PROJECT/local_settings.example.py" "$PROJECT/hsreplaynet/local_settings.py"
