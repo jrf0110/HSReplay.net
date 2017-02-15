df134c49ca465f76b002a65096b29e386d010a69
Alexander Zeier <alex@hearthsim.net>
2017-02-15 18:51:26 +0100

    CardIcon: Add sizing, marks and tooltips

--- hsreplaynet/static/scripts/src/components/CardIcon.tsx
+++ hsreplaynet/static/scripts/src/components/CardIcon.tsx
@@ -1,18 +1,76 @@
 import * as React from "react";
 
-interface CardIconProps extends React.ClassAttributes<CardIcon> {
-	cardId: string;
+interface CardIconState {
+	clientX?: number;
+	clientY?: number;
+	hovering?: boolean;
 }
 
-export default class CardIcon extends React.Component<CardIconProps, any> {
+interface CardIconProps extends React.ClassAttributes<CardIcon> {
+	cardId: string;
+	size?: number;
+	mark?: string;
+	markStyle?: any;
+}
+
+export default class CardIcon extends React.Component<CardIconProps, CardIconState> {
+	readonly baseSize = 34;
+	readonly baseBackgroundWidth = 134;
+	readonly baseOffset = -75;
+
+	constructor(props: CardIconProps, state: CardIconState) {
+		super(props, state);
+		this.state = {
+			clientX: 0,
+			clientY: 0,
+			hovering: false,
+		}
+	}
+
 	render(): JSX.Element {
 		if (this.props.cardId) {
-			const image = "url(https://art.hearthstonejson.com/v1/tiles/" + this.props.cardId + ".png)";
+			const size = this.props.size || this.baseSize;
+			const style = {
+				backgroundImage: "url(https://art.hearthstonejson.com/v1/tiles/" + this.props.cardId + ".png)",
+				backgroundPositionX: this.baseOffset * (size / this.baseSize) + "px",
+				backgroundSize: this.baseBackgroundWidth * (size / this.baseSize) + "px " + size + "px",
+				height: size + "px",
+				width: size + "px",
+			};
+
+			let mark = null;
+			if (this.props.mark) {
+				mark = <span style={this.props.markStyle}>{this.props.mark}</span>;
+			}
+
+			let tooltip = null;
+			if (this.state.hovering) {
+				const imageStyle = {
+					bottom: Math.min(this.state.clientY - 350, 0)
+				};
+				const left = this.state.clientX < window.innerWidth / 2;
+				imageStyle[left ? "left" : "right"] = "40px";
+
+				tooltip = (
+						<img
+							className="card-image"
+							height={350}
+							src={"http://media.services.zam.com/v1/media/byName/hs/cards/enus/" + this.props.cardId + ".png"}
+							style={imageStyle}
+						/>
+				);
+			}
+
 			return (
 				<div
 					className="card-icon"
-					style={{backgroundImage: image}}
-				/>
+					style={style}
+					onMouseEnter={(e) => this.setState({hovering: true, clientX: e.clientX, clientY: e.clientY})}
+					onMouseLeave={() => this.setState({hovering: false})}
+				>
+					{mark}
+					{tooltip}
+				</div>
 			);
 		}
 	}
