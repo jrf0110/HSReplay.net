01df4ff2bccb355e43bcb92010b95b050d706878
Jerome Leclanche <jerome@leclan.ch>
2017-02-17 03:51:38 +0200

    Use new-style syntax for admin.site.urls

--- hsreplaynet/admin/urls.py
+++ hsreplaynet/admin/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 
 urlpatterns = [
-	url(r"^", include(admin.site.urls)),
+	url(r"^", admin.site.urls),
 	url(r"^loginas/", include("loginas.urls")),
 	url(r"^rq/", include("django_rq_dashboard.urls")),
 ]
