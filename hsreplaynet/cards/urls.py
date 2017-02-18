bf78a9514fb9673751773a76e33452c0d20fc3fb
Jerome Leclanche <jerome@leclan.ch>
2017-02-18 06:01:14 +0200

    Move card list to /cards/

--- hsreplaynet/cards/urls.py
+++ hsreplaynet/cards/urls.py
@@ -6,12 +6,12 @@ from .views import (
 
 
 urlpatterns = [
+	url(r"^$", discover, name="card_discover"),
 	url(r"^winrates/$", winrates, name="deck_winrates"),
 	url(r"^counters/$", counters, name="deck_counters"),
 	url(r"^archetypes/$", archetypes, name="deck_archetypes"),
 	url(r"^canonicals/$", canonicals, name="canonical_decks"),
 	url(r"^popular/$", popular_cards, name="popular_cards"),
-	url(r"^discover/$", discover, name="card_discover"),
 	url(r"^(?P<card_id>\w+)$", carddetail, name="card_detail"),
 	url(r"^decks/discover/$", deck_discover, name="deck_discover"),
 	url(r"^decks/(?P<deck_id>\w+)$", deckdetail, name="deck_detail"),
