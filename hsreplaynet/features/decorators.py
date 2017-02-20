05c55212b7766ff0075af189c0d509608fb47e45
Jerome Leclanche <jerome@leclan.ch>
2017-02-20 06:54:07 +0200

    Fix a feature flag crash when the feature does not exist

--- hsreplaynet/features/decorators.py
+++ hsreplaynet/features/decorators.py
@@ -1,8 +1,8 @@
 from functools import wraps
 from django.core.exceptions import PermissionDenied
 from django.views.generic import View
-from .models import Feature
 from hsreplaynet.utils.instrumentation import error_handler
+from .models import Feature
 
 
 def view_requires_feature_access(feature_name):
@@ -10,24 +10,22 @@ def view_requires_feature_access(feature_name):
 	def decorator(view_func):
 		@wraps(view_func)
 		def wrapper(arg1, *args, **kwargs):
+			if issubclass(arg1.__class__, View):
+				# If we are using class based views the request is in args
+				request = args[0]
+			else:
+				request = arg1
 
 			try:
 				feature = Feature.objects.get(name=feature_name)
-
-				if issubclass(arg1.__class__, View):
-					# If we are using class based views the request is in args
-					request = args[0]
-				else:
-					request = arg1
-
-				is_enabled = feature.enabled_for_user(request.user)
-
 			except Feature.DoesNotExist as e:
 				error_handler(e)
 				# Missing features are treated as if they are set to
 				# FeatureStatus.STAFF_ONLY. This occurs when new feature code is deployed
 				# before the DB is updated
-				is_enabled = arg1.user.is_staff
+				is_enabled = request.user.is_staff
+			else:
+				is_enabled = feature.enabled_for_user(request.user)
 
 			if is_enabled:
 				return view_func(arg1, *args, **kwargs)
