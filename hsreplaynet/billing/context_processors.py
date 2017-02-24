5c3fba9b5e4037b8400c078b6e8f88e561167ece
Jerome Leclanche <jerome@leclan.ch>
2017-02-24 23:17:13 +0200

    Always display the premium modal if ?premium-modal is in the params

--- hsreplaynet/billing/context_processors.py
+++ hsreplaynet/billing/context_processors.py
@@ -4,6 +4,8 @@ Context processors for billing/premium purposes
 
 
 def premium(request):
+	is_premium = request.user.is_authenticated and request.user.is_premium
 	return {
-		"premium": request.user.is_authenticated and request.user.is_premium,
+		"premium": is_premium,
+		"show_premium_modal": not is_premium and "premium-modal" in request.GET,
 	}
