602bbf8a227ff40bc817b1bff6c49a5b3c186c3c
Alexander Zeier <alex@hearthsim.net>
2017-03-03 14:24:17 +0100

    Make InfoboxFilterGroup collapsible

--- hsreplaynet/static/scripts/src/components/InfoboxFilterGroup.tsx
+++ hsreplaynet/static/scripts/src/components/InfoboxFilterGroup.tsx
@@ -2,13 +2,27 @@ import * as React from "react";
 
 interface InfoboxFilterGroupProps extends React.ClassAttributes<InfoboxFilterGroup> {
 	classNames?: string[];
+	collapsed?: boolean;
+	collapsible?: boolean;
 	deselectable?: boolean;
+	header?: string;
 	locked?: boolean;
 	onClick: (value: string, sender: string) => void;
 	selectedValue: string | string[];
 }
 
-export default class InfoboxFilterGroup extends React.Component<InfoboxFilterGroupProps, void> {
+interface InfoboxFilterGroupState {
+	collapsed?: boolean;
+}
+
+export default class InfoboxFilterGroup extends React.Component<InfoboxFilterGroupProps, InfoboxFilterGroupState> {
+	constructor(props: InfoboxFilterGroupProps, state: InfoboxFilterGroupState) {
+		super(props, state);
+		this.state = {
+			collapsed: props.collapsed,
+		}
+	}
+
 	render(): JSX.Element {
 		const selected = value => {
 			if (!this.props.selectedValue) {
@@ -30,9 +44,31 @@ export default class InfoboxFilterGroup extends React.Component<InfoboxFilterGro
 			});
 		};
 
+		let header = null;
+		if (this.props.header) {
+			let icon = null;
+			let headerClassName = null;
+			if (this.props.collapsed || this.props.collapsible) {
+				headerClassName = "collapsible";
+				if (this.state.collapsed) {
+					icon = <span className="glyphicon glyphicon-menu-down"/>
+				}
+				else {
+					icon = <span className="glyphicon glyphicon-menu-up"/>
+				}
+			}
+			header = (
+				<h2 className={headerClassName} onClick={() => this.setState({collapsed: !this.state.collapsed})}>
+					{icon}
+					{this.props.header}
+				</h2>
+			);
+		}
+
 		return (
 			<ul className={this.props.classNames && this.props.classNames.join(" ")}>
-				{React.Children.map(this.props.children, cloneWidthProps)}
+				{header}
+				{!this.state.collapsed && React.Children.map(this.props.children, cloneWidthProps)}
 			</ul>
 		);
 	}
