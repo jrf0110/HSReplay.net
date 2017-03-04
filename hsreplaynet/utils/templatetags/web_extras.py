8e65760f17d9972f24f7768a7973bbcd2bf0339c
Jerome Leclanche <jerome@leclan.ch>
2017-03-04 22:08:53 +0200

    Properly implement deck meta tags on deck pages

--- hsreplaynet/utils/templatetags/web_extras.py
+++ hsreplaynet/utils/templatetags/web_extras.py
@@ -3,9 +3,6 @@ from django import template
 from django.conf import settings
 from django.contrib.staticfiles.templatetags.staticfiles import static
 from django.utils.safestring import mark_safe
-from hearthstone.enums import CardType
-from hsreplaynet.cards.models import Card, Deck
-from hsreplaynet.cards.archetypes import guess_class
 from hsreplaynet.games.models import GameReplay
 
 
@@ -67,34 +64,3 @@ def static_absolute(context, value):
 	if not re.match("[a-z]([a-z0-9+-.])*:", value, re.IGNORECASE):
 		value = "%s://%s%s" % (request.scheme, request.get_host(), value)
 	return value
-
-
-@register.inclusion_tag("deck_list_meta_block.html", takes_context=True)
-def render_deck_list_meta_for_digest_param(context):
-	request = context.request
-	deck_digest = request.GET.get("deck_digest", "")
-	result = {
-		"render_meta_tag": False
-	}
-
-	if deck_digest:
-		deck = Deck.objects.filter(digest=deck_digest).first()
-		if deck:
-			class_guess = guess_class(deck)
-			if class_guess:
-				hero_card = Card.objects.filter(
-					card_class=class_guess,
-					type=CardType.HERO
-				).first()
-				if hero_card:
-					result["render_meta_tag"] = True
-					result["card_ids"] = ",".join(deck.card_id_list())
-					result["hero_id"] = str(hero_card.id)
-
-					if deck.archetype:
-						name = deck.archetype.name
-					else:
-						name = "Deck"
-					result["deck_name"] = "HSReplay.net %s" % name
-
-	return result
