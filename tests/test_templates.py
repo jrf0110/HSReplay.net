35bd363330d4e55f0594db0297feed72b38c5dac
Jerome Leclanche <jerome@leclan.ch>
2017-03-04 22:17:04 +0200

    Add a test to ensure templates compile

---
+++ tests/test_templates.py
@@ -0,0 +1,13 @@
+import os
+from django.template import Template
+
+
+def test_compile_templates(settings):
+	for template_dir in settings.TEMPLATES[0]["DIRS"]:
+		for basepath, dirs, filenames in os.walk(template_dir):
+			for filename in filenames:
+				path = os.path.join(basepath, filename)
+				with open(path, "r") as f:
+					# This will fail if the template cannot compile
+					t = Template(f.read())
+					assert t
