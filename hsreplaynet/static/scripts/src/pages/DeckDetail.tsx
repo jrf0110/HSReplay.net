cf03f7ff02b5f812db7bf7d0334c4f41ac77dffd
Alexander Zeier <alex@hearthsim.net>
2017-03-06 18:47:16 +0100

    Move mulligan winrate behind premium

--- hsreplaynet/static/scripts/src/pages/DeckDetail.tsx
+++ hsreplaynet/static/scripts/src/pages/DeckDetail.tsx
@@ -473,8 +473,8 @@ export default class DeckDetail extends React.Component<DeckDetailProps, DeckDet
 
 		const tableHeaders = [
 			{key: "decklist", text: "Card", defaultSortDirection: "ascending" as SortDirection},
+			{key: "keep_percentage", text: "Kept", infoHeader: "Kept", infoText: "Percentage the card was kept when presented during mulligan." },
 			{key: "opening_hand_win_rate", text: "Mulligan WR", infoHeader: "Mulligan Winrate", infoText: "Winrate when the card ends up in the opening hand." },
-			{key: "keep_percentage", text: "Kept", infoHeader: "Kept", infoText: "Percentage card was kept when presented during mulligan." },
 			{key: "win_rate_when_drawn", text: "Drawn WR", infoHeader: "Drawn Winrate", infoText: "Average winrate of games where the card was drawn at any point." },
 			{key: "win_rate_when_played", text: "Played WR", infoHeader: "Played Winrate", infoText: "Average winrate of games where the card was played at any point." },
 			{key: "avg_turns_in_hand", text: "Turns held", infoHeader: "Turns held", infoText: "Average number of turn the card is held in hand."},
@@ -512,16 +512,12 @@ export default class DeckDetail extends React.Component<DeckDetailProps, DeckDet
 				statusIcon = <span className="glyphicon glyphicon-warning-sign" title="Low number of data points" />;
 			}
 			cols.push(
-				<td className="winrate-cell" style={{color: mulligan.color}}>
-					{mulligan.tendencyStr + (+row["opening_hand_win_rate"]).toFixed(1) + "%"}
-					{statusIcon}
-				</td>,
 				<td>{(+row["keep_percentage"]).toFixed(1) + "%"}</td>,
 			);
 			if (!this.props.userIsPremium) {
 				if (firstRow) {
 					cols.push(
-						<td colSpan={4} rowSpan={rowCount} style={{background: "rgba(0,0,0,0.1)", textAlign: "center"}}>
+						<td colSpan={5} rowSpan={rowCount} style={{background: "rgba(0,0,0,0.1)", textAlign: "center"}}>
 							<PremiumWrapper isPremium>
 								<h1 style={{padding: "50px"}}>HearthSim Premium</h1>
 								<ul style={{listStyleType: "none", padding: 0}}>
@@ -547,6 +543,10 @@ export default class DeckDetail extends React.Component<DeckDetailProps, DeckDet
 			}
 			else {
 				cols.push(
+					<td className="winrate-cell" style={{color: mulligan.color}}>
+						{mulligan.tendencyStr + (+row["opening_hand_win_rate"]).toFixed(1) + "%"}
+						{statusIcon}
+					</td>,
 					<td className="winrate-cell" style={{color: drawn.color}}>{drawn.tendencyStr + (+row["win_rate_when_drawn"]).toFixed(1) + "%"}</td>,
 					<td className="winrate-cell" style={{color: played.color}}>{played.tendencyStr + (+row["win_rate_when_played"]).toFixed(1) + "%"}</td>,
 					<td>{(+row["avg_turns_in_hand"]).toFixed(1)}</td>,
