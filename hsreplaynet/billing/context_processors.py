0f5f95b0c430dc43263703548c1808d9504aa435
Jerome Leclanche <jerome@leclan.ch>
2017-03-06 17:31:31 +0200

    Check for livemode when filtering for plans

--- hsreplaynet/billing/context_processors.py
+++ hsreplaynet/billing/context_processors.py
@@ -12,10 +12,11 @@ STRIPE_DEBUG = not STRIPE_LIVE_MODE and settings.DEBUG
 
 def premium(request):
 	is_premium = request.user.is_authenticated and request.user.is_premium
+	plans = Plan.objects.filter(livemode=STRIPE_LIVE_MODE)
 	return {
 		"premium": is_premium,
 		"show_premium_modal": not is_premium and "premium-modal" in request.GET,
-		"monthly_plan": Plan.objects.filter(stripe_id=settings.MONTHLY_PLAN_ID).first(),
-		"semiannual_plan": Plan.objects.filter(stripe_id=settings.SEMIANNUAL_PLAN_ID).first(),
+		"monthly_plan": plans.filter(stripe_id=settings.MONTHLY_PLAN_ID).first(),
+		"semiannual_plan": plans.filter(stripe_id=settings.SEMIANNUAL_PLAN_ID).first(),
 		"stripe_debug": STRIPE_DEBUG,
 	}
