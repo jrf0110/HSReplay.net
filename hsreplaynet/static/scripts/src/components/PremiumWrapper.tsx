69b1fc1f3f32e043061be0494f7dc501ba5ec53f
Benedict Etzel <benedict@hearthsim.net>
2017-03-08 12:58:36 +0100

    Improve PremiumWrapper
    Closes #181. Refs #215.

--- hsreplaynet/static/scripts/src/components/PremiumWrapper.tsx
+++ hsreplaynet/static/scripts/src/components/PremiumWrapper.tsx
@@ -10,43 +10,115 @@ interface PremiumWrapperProps extends React.ClassAttributes<PremiumWrapper> {
 	infoContent?: string;
 }
 
-export default class PremiumWrapper extends React.Component<PremiumWrapperProps, any> {
-	private touchCount: number = 0;
-	
-	render(): JSX.Element {
-		let info = null;
-		if (!this.props.isPremium) {
-			info = (
-				<div className="premium-info">
-					<h4>Premium only</h4>
-					<span>Click here for more info!</span>
-				</div>
-			);
-		}
+interface PremiumWrapperState {
+	hovering?: boolean;
+	triggered?: boolean; // inferred hover
+	touchCount?: number;
+}
 
+const key = "hsreplaynet_premium_wrappers";
+
+export default class PremiumWrapper extends React.Component<PremiumWrapperProps, PremiumWrapperState> {
+
+	constructor(props: PremiumWrapperProps, context: any) {
+		super(props, context);
+		this.state = {
+			hovering: false,
+			touchCount: 0,
+		};
+	}
+
+	public trigger() {
+		this.setState({
+			triggered: true,
+			touchCount: 0,
+		});
+	}
+
+	public release() {
+		this.setState({triggered: false});
+	}
+
+	componentDidMount() {
+		// register to global list of premium wrappers
+		if (typeof window[key] === "undefined") {
+			window[key] = [];
+		}
+		window[key].push(this);
+	}
+
+	componentWillUnmount() {
+		window[key] = window[key].filter((component: PremiumWrapper) => component != this);
+	}
+
+	componentWillUpdate(nextProps: PremiumWrapperProps, nextState: PremiumWrapperState) {
+		if (nextState.hovering === this.state.hovering) {
+			return;
+		}
+		// hover is starting or ending
+		window[key].forEach((wrapper: PremiumWrapper) => {
+			if (wrapper === this) {
+				return;
+			}
+			if (nextState.hovering) {
+				wrapper.trigger();
+			}
+			else {
+				wrapper.release();
+			}
+		});
+	}
+
+	render(): JSX.Element {
 		let infoIcon = null;
 		if (this.props.infoHeader) {
-			infoIcon = <InfoIcon header={this.props.infoHeader} content={this.props.infoContent} />
+			infoIcon = <InfoIcon
+				header={this.props.infoHeader}
+				content={this.props.infoContent}
+			/>;
+		}
+
+		const classNames = ["premium-wrapper"];
+		if (this.shouldAppear()) {
+			classNames.push("visible");
+		}
+		if (this.state.hovering || this.state.triggered) {
+			classNames.push("hovering");
 		}
 
 		return (
 			<div
-				className="premium-wrapper"
-				onTouchStart={() => this.touchCount++}
-				onClick={(e) => (this.touchCount % 2 === 0) && !this.props.isPremium && showModal()}
-				onMouseEnter={() => !this.props.isPremium && this.setState({showInfo: true, fadeOut: false})}
-				onMouseLeave={() => {
-					if(!this.props.isPremium) {
-						this.touchCount = 0;
-						this.setState({fadeOut: true});
+				className={classNames.join(" ")}
+				onTouchStart={() => this.setState({hovering: true, touchCount: this.state.touchCount + 1})}
+				onTouchCancel={() => this.setState({hovering: false})}
+				onClick={(e) => {
+					if(!this.shouldAppear()) {
+						return;
 					}
+					if(this.state.touchCount % 2 === 1) {
+						return;
+					}
+					showModal();
 				}}
+				onMouseEnter={() => this.setState({hovering: true})}
+				onMouseLeave={() => this.setState({hovering: false, touchCount: 0})}
 			>
-				<img className="premium-icon" src={STATIC_URL + "images/ranked-medals/Medal_Ranked_Legend.png"} style={this.props.iconStyle}/>
+				<img
+					className="premium-icon"
+					src={STATIC_URL + "images/ranked-medals/Medal_Ranked_Legend.png"}
+					style={this.props.iconStyle}
+				/>
 				{infoIcon}
-				{info}
+				<div className="premium-info">
+					<h4><span className="text-premium">Premium</span> only</h4>
+					{this.state.touchCount > 0 ? <span>Tap for more details…</span> : null}
+				</div>
 				{this.props.children}
 			</div>
 		);
+	}
+
+	protected shouldAppear() {
+		return !this.props.isPremium;
 	}
 }
