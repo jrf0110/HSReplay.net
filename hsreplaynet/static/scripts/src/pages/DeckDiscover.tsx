57feda6b27dcab8043698f3ce3c00a6e90f61c28
Benedict Etzel <benedict@hearthsim.net>
2017-03-10 11:01:20 +0100

    Clean up DeckDiscover.tsx

--- hsreplaynet/static/scripts/src/pages/DeckDiscover.tsx
+++ hsreplaynet/static/scripts/src/pages/DeckDiscover.tsx
@@ -7,14 +7,14 @@ import InfoboxFilter from "../components/InfoboxFilter";
 import InfoboxFilterGroup from "../components/InfoboxFilterGroup";
 import Pager from "../components/Pager";
 import PremiumWrapper from "../components/PremiumWrapper";
-import QueryManager from "../QueryManager";
 import ResetHeader from "../components/ResetHeader";
-import {DeckObj, TableData, GameMode, MyDecks, RankRange, Region, TimeFrame} from "../interfaces";
 import {cardSorting, getDustCost, toTitleCase} from "../helpers";
+import {DeckObj, GameMode, MyDecks, RankRange, Region, TableData, TimeFrame} from "../interfaces";
+import QueryManager from "../QueryManager";
 import {
-	genCacheKey, QueryMap, getQueryMapArray, getQueryMapFromLocation, queryMapHasChanges,
-	setLocationQueryString, setQueryMap, toQueryString, getQueryMapDiff
-} from "../QueryParser"
+	genCacheKey, getQueryMapArray, getQueryMapDiff, getQueryMapFromLocation, QueryMap,
+	queryMapHasChanges, setLocationQueryString, setQueryMap, toQueryString,
+} from "../QueryParser";
 
 interface DeckDiscoverState {
 	cardSearchExcludeKey?: number;
@@ -46,7 +46,7 @@ export default class DeckDiscover extends React.Component<DeckDiscoverProps, Dec
 		sortBy: "popularity",
 		sortDirection: "descending",
 		timeRange: "LAST_30_DAYS",
-	}
+	};
 
 	private readonly allowedValues = {
 		gameType: ["RANKED_STANDARD", "RANKED_WILD"],
@@ -54,14 +54,14 @@ export default class DeckDiscover extends React.Component<DeckDiscoverProps, Dec
 		region: [],
 		opponentClass: [],
 		timeRange: ["LAST_30_DAYS"],
-	}
+	};
 
 	private readonly allowedValuesPremium = {
 		gameType: ["RANKED_STANDARD", "RANKED_WILD"],
 		rankRange: ["LEGEND_THROUGH_TEN"],
 		region: [],
 		timeRange: ["CURRENT_SEASON", "LAST_3_DAYS", "LAST_7_DAYS", "LAST_30_DAYS"],
-	}
+	};
 
 	constructor(props: DeckDiscoverProps, state: DeckDiscoverState) {
 		super(props, state);
@@ -73,7 +73,7 @@ export default class DeckDiscover extends React.Component<DeckDiscoverProps, Dec
 			myDecks: null,
 			queryMap: getQueryMapFromLocation(this.defaultQueryMap, this.getAllowedValues()),
 			showFilters: false,
-		}
+		};
 		this.fetch();
 	}
 
@@ -82,7 +82,7 @@ export default class DeckDiscover extends React.Component<DeckDiscoverProps, Dec
 	}
 
 	componentDidUpdate(prevProps: DeckDiscoverProps, prevState: DeckDiscoverState) {
-		const cacheKey = genCacheKey(this)
+		const cacheKey = genCacheKey(this);
 		const prevCacheKey = genCacheKey(this, prevState);
 		if (cacheKey !== prevCacheKey) {
 			const deckData = this.state.deckData.get(cacheKey);
@@ -101,7 +101,7 @@ export default class DeckDiscover extends React.Component<DeckDiscoverProps, Dec
 					cards.push(card);
 				}
 			});
-			cards.sort(cardSorting)
+			cards.sort(cardSorting);
 			this.setState({cards});
 		}
 	}
@@ -123,7 +123,7 @@ export default class DeckDiscover extends React.Component<DeckDiscoverProps, Dec
 
 	render(): JSX.Element {
 		const queryMap = Object.assign({}, this.state.queryMap);
-		
+
 		const selectedClass = queryMap["playerClass"];
 		const selectedOpponent = queryMap["opponentClass"];
 		const decks: DeckObj[] = [];
@@ -132,19 +132,28 @@ export default class DeckDiscover extends React.Component<DeckDiscoverProps, Dec
 			if (deckData && deckData !== "loading" && deckData !== "error") {
 				const deckElements = [];
 				const data = deckData.series.data;
-				const includedCards = getQueryMapArray(queryMap, "includedCards").map(dbfId => this.props.cardData.fromDbf(dbfId));
-				const excludedCards = getQueryMapArray(queryMap, "excludedCards").map(dbfId => this.props.cardData.fromDbf(dbfId));
-				Object.keys(data).forEach(key => {
+				const includedCards = getQueryMapArray(queryMap, "includedCards").map(
+					(dbfId) => this.props.cardData.fromDbf(dbfId));
+				const excludedCards = getQueryMapArray(queryMap, "excludedCards").map(
+					(dbfId) => this.props.cardData.fromDbf(dbfId));
+				Object.keys(data).forEach((key) => {
 					if (selectedClass === "ALL" || selectedClass === key) {
-						data[key].forEach(deck => {
+						data[key].forEach((deck) => {
 							const cards = JSON.parse(deck["deck_list"]);
-							const deckList = cards.map(c => {return {card: this.props.cardData.fromDbf(c[0]), count: c[1]}});
-							if (!includedCards.length || includedCards.every(card => deckList.some(cardObj => cardObj.card.id === card.id))) {
-								if (!excludedCards.length || !excludedCards.some(card => deckList.some(cardObj => cardObj.card.id === card.id))) {
+							const deckList = cards.map((c) => {
+								return {card: this.props.cardData.fromDbf(c[0]), count: c[1]};
+							});
+							if (!includedCards.length || includedCards.every(
+									(card) => deckList.some((cardObj) => cardObj.card.id === card.id))) {
+								if (!excludedCards.length || !excludedCards.some(
+										(card) => deckList.some((cardObj) => cardObj.card.id === card.id))) {
 									const costSum = deckList.reduce((a, b) => a + b.card.cost * b.count, 0);
 									if (!queryMap["personal"] || this.state.myDecks && this.state.myDecks[deck["deck_id"]]) {
 										deck["cards"] = deckList;
-										deck["dust_cost"] = deckList.reduce((a, b) => a + getDustCost(b.card) * b.count, 0);
+										deck["dust_cost"] = deckList.reduce((
+											a,
+											b,
+										) => a + getDustCost(b.card) * b.count, 0);
 										deck["mana_cost"] = costSum;
 										deck["player_class"] = key;
 										deckElements.push(deck);
@@ -158,8 +167,8 @@ export default class DeckDiscover extends React.Component<DeckDiscoverProps, Dec
 				const winrateField = selectedOpponent === "ALL" ? "win_rate" : "win_rate_vs_" + selectedOpponent;
 				const numGamesField = selectedOpponent === "ALL" ? "total_games" : "total_games_vs_" + selectedOpponent;
 				let sortProp = queryMap["sortBy"];
-				switch(sortProp) {
-					case "winrate": 
+				switch (sortProp) {
+					case "winrate":
 						sortProp = winrateField;
 						break;
 					case "popularity":
@@ -173,7 +182,7 @@ export default class DeckDiscover extends React.Component<DeckDiscoverProps, Dec
 				const direction = queryMap["sortDirection"] === "descending" ? 1 : -1;
 				deckElements.sort((a, b) => (b[sortProp] - a[sortProp]) * direction);
 
-				deckElements.forEach(deck => {
+				deckElements.forEach((deck) => {
 					decks.push({
 						cards: deck.cards,
 						deckId: deck.deck_id,
@@ -202,16 +211,23 @@ export default class DeckDiscover extends React.Component<DeckDiscoverProps, Dec
 				</div>
 			);
 		}
-		else if(decks.length === 0) {
+		else if (decks.length === 0) {
 			if (this.state.queryMap["personal"]) {
 				content = (
 					<div className="content-message">
-						<h2>None of your decks are here :(</h2><br/>
+						<h2>None of your decks are here :(</h2><br />
 						<h3>We can fix that!</h3>
-						<p>Step 1: If you haven't already, <a href="/downloads/">download Hearthstone Deck Tracker</a></p>
-						<p>Step 2: Play some games with a deck from the list and it will show up!</p><br/>
-						<p><i>Protip: Every deck has a "Copy deck to HDT" button!</i></p><br/>
-						<button className="btn btn-default" type="button" onClick={() => setQueryMap(this, "personal", null)}>Back to the decks</button>
+						<p>
+							Step 1: If you haven't already, <a href="/downloads/">download Hearthstone Deck Tracker</a></p>
+							<p>Step 2: Play some games with a deck from the list and it will show up!</p><br />
+							<p><i>Protip: Every deck has a "Copy deck to HDT" button!</i></p><br />
+							<button
+								className="btn btn-default"
+								type="button"
+								onClick={() => setQueryMap(this, "personal", null)}
+							>
+								Back to the decks
+							</button>
 					</div>
 				);
 			}
@@ -219,7 +235,9 @@ export default class DeckDiscover extends React.Component<DeckDiscoverProps, Dec
 				content = (
 					<div className="content-message">
 						<h2>No decks found</h2>
-						<button className="btn btn-default" type="button" onClick={() => this.resetFilters()}>Reset filters</button>
+						<button className="btn btn-default" type="button" onClick={() => this.resetFilters()}>
+							Reset filters
+						</button>
 					</div>
 				);
 			}
@@ -248,7 +266,7 @@ export default class DeckDiscover extends React.Component<DeckDiscoverProps, Dec
 		}
 
 		const filterClassNames = ["infobox full-sm"];
-		const contentClassNames = ["deck-list-wrapper"]
+		const contentClassNames = ["deck-list-wrapper"];
 		if (!this.state.showFilters) {
 			filterClassNames.push("hidden-xs hidden-sm");
 		}
@@ -257,7 +275,11 @@ export default class DeckDiscover extends React.Component<DeckDiscoverProps, Dec
 		}
 
 		const backButton = (
-			<button className="btn btn-primary btn-full visible-sm visible-xs" type="button" onClick={() => this.setState({showFilters: false})}>
+			<button
+				className="btn btn-primary btn-full visible-sm visible-xs"
+				type="button"
+				onClick={() => this.setState({showFilters: false})}
+			>
 				Back to deck list
 			</button>
 		);
@@ -273,11 +295,14 @@ export default class DeckDiscover extends React.Component<DeckDiscoverProps, Dec
 			<div className="deck-discover">
 				<div className={filterClassNames.join(" ")} id="deck-discover-infobox">
 					{backButton}
-					<ResetHeader onReset={() => this.resetFilters()} showReset={queryMapHasChanges(this.state.queryMap, this.getDefeaultQueryMap())}>
+					<ResetHeader
+						onReset={() => this.resetFilters()}
+						showReset={queryMapHasChanges(this.state.queryMap, this.getDefeaultQueryMap())}
+					>
 						Decks
 					</ResetHeader>
 					<h2>Class</h2>
-					<ClassFilter 
+					<ClassFilter
 						filters="All"
 						hideAll
 						minimal
@@ -291,7 +316,7 @@ export default class DeckDiscover extends React.Component<DeckDiscoverProps, Dec
 						infoContent="See at a glance how various decks perform against a specific class!"
 					>
 						<h2>Opponent class</h2>
-						<ClassFilter 
+						<ClassFilter
 							filters="All"
 							hideAll
 							minimal
@@ -304,25 +329,32 @@ export default class DeckDiscover extends React.Component<DeckDiscoverProps, Dec
 					<CardSearch
 						key={"cardinclude" + this.state.cardSearchIncludeKey}
 						availableCards={this.state.cards}
-						onCardsChanged={(cards) => setQueryMap(this, "includedCards", cards.map(card => card.dbfId).join(","))}
-						selectedCards={this.props.cardData && queryMap["includedCards"] && queryMap["includedCards"].split(",").map(dbfId => this.props.cardData.fromDbf(dbfId))}
+						onCardsChanged={(cards) => setQueryMap(this, "includedCards", cards.map((card) => card.dbfId).join(","))}
+						selectedCards={this.props.cardData && queryMap["includedCards"] && queryMap["includedCards"].split(",").map((dbfId) => this.props.cardData.fromDbf(dbfId))}
 					/>
 					<h2>Exclude cards</h2>
 					<CardSearch
 						key={"cardexclude" + this.state.cardSearchExcludeKey}
 						availableCards={this.state.cards}
-						onCardsChanged={(cards) => setQueryMap(this, "excludedCards", cards.map(card => card.dbfId).join(","))}
-						selectedCards={this.props.cardData && queryMap["excludedCards"] && queryMap["excludedCards"].split(",").map(dbfId => this.props.cardData.fromDbf(dbfId))}
+						onCardsChanged={(cards) => setQueryMap(this, "excludedCards", cards.map((card) => card.dbfId).join(","))}
+						selectedCards={this.props.cardData && queryMap["excludedCards"] && queryMap["excludedCards"].split(",").map((dbfId) => this.props.cardData.fromDbf(dbfId))}
 					/>
 					<h2>Personal</h2>
-					<InfoboxFilterGroup deselectable selectedValue={this.state.queryMap["personal"]} onClick={(value) => setQueryMap(this, "personal", value)}>
+					<InfoboxFilterGroup
+						deselectable
+						selectedValue={this.state.queryMap["personal"]}
+						onClick={(value) => setQueryMap(this, "personal", value)}
+					>
 						<InfoboxFilter value="true" disabled={personalDisabled}>
 							I have played (last 30 days)
 							{loginLink}
 						</InfoboxFilter>
 					</InfoboxFilterGroup>
 					<h2>Mode</h2>
-					<InfoboxFilterGroup selectedValue={this.state.queryMap["gameType"]} onClick={(value) => setQueryMap(this, "gameType", value)}>
+					<InfoboxFilterGroup
+						selectedValue={this.state.queryMap["gameType"]}
+						onClick={(value) => setQueryMap(this, "gameType", value)}
+					>
 						<InfoboxFilter value="RANKED_STANDARD">Standard</InfoboxFilter>
 						<InfoboxFilter value="RANKED_WILD">Wild</InfoboxFilter>
 					</InfoboxFilterGroup>
@@ -332,7 +364,11 @@ export default class DeckDiscover extends React.Component<DeckDiscoverProps, Dec
 						infoContent="Want to see what decks are hot right now? Look at data from a time frame of your choosing!"
 					>
 						<h2>Time frame</h2>
-						<InfoboxFilterGroup locked={!this.props.userIsPremium} selectedValue={this.state.queryMap["timeRange"]} onClick={(value) => setQueryMap(this, "timeRange", value)}>
+						<InfoboxFilterGroup
+							locked={!this.props.userIsPremium}
+							selectedValue={this.state.queryMap["timeRange"]}
+							onClick={(value) => setQueryMap(this, "timeRange", value)}
+						>
 							<InfoboxFilter value="CURRENT_SEASON">Current Season</InfoboxFilter>
 							<InfoboxFilter value="LAST_3_DAYS">Last 3 days</InfoboxFilter>
 							<InfoboxFilter value="LAST_7_DAYS">Last 7 days</InfoboxFilter>
@@ -345,7 +381,11 @@ export default class DeckDiscover extends React.Component<DeckDiscoverProps, Dec
 						infoContent="Ready to climb the ladder? Check out how decks perform in the higher ranks!"
 					>
 						<h2>Rank range</h2>
-						<InfoboxFilterGroup locked={!this.props.userIsPremium} selectedValue={this.state.queryMap["rankRange"]} onClick={(value) => setQueryMap(this, "rankRange", value)}>
+						<InfoboxFilterGroup
+							locked={!this.props.userIsPremium}
+							selectedValue={this.state.queryMap["rankRange"]}
+							onClick={(value) => setQueryMap(this, "rankRange", value)}
+						>
 							<InfoboxFilter value="LEGEND_THROUGH_TEN">Legend–10</InfoboxFilter>
 							<InfoboxFilter value="ALL">Legend–25</InfoboxFilter>
 						</InfoboxFilterGroup>
@@ -353,7 +393,9 @@ export default class DeckDiscover extends React.Component<DeckDiscoverProps, Dec
 					{backButton}
 				</div>
 				<div className={contentClassNames.join(" ")}>
-					<div className="alert alert-info" role="alert">Decks require at least 1000 recorded games in the selected time frame to be listed</div>
+					<div className="alert alert-info" role="alert">
+						Decks require at least 1000 recorded games in the selected time frame to be listed.
+					</div>
 					<div className="opponent-filter-wrapper">
 					</div>
 					<button
@@ -361,7 +403,7 @@ export default class DeckDiscover extends React.Component<DeckDiscoverProps, Dec
 						type="button"
 						onClick={() => this.setState({showFilters: true})}
 					>
-						<span className="glyphicon glyphicon-filter"/>
+						<span className="glyphicon glyphicon-filter" />
 						Filters
 					</button>
 					{content}
@@ -383,7 +425,7 @@ export default class DeckDiscover extends React.Component<DeckDiscoverProps, Dec
 		const query = this.props.userIsPremium ? "list_decks_by_opponent_win_rate" : "list_decks_by_win_rate";
 		this.queryManager.fetch(
 			"/analytics/query/" + query + "?" + toQueryString(params),
-			(data) => this.setState({deckData: this.state.deckData.set(cacheKey, data)})
+			(data) => this.setState({deckData: this.state.deckData.set(cacheKey, data)}),
 		);
 
 		if (!this.state.myDecks) {
