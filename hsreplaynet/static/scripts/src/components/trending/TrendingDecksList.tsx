7be29ec0670f621564267b9befe6f7b8bc5910a8
Alexander Zeier <alex@hearthsim.net>
2017-03-12 22:25:11 +0100

    Refactor DeckSpotlight for new query system

---
+++ hsreplaynet/static/scripts/src/components/trending/TrendingDecksList.tsx
@@ -0,0 +1,35 @@
+import * as React from "react";
+import CardData from "../../CardData";
+import { DeckObj, TableData } from "../../interfaces";
+import DeckList from "../DeckList";
+
+interface TrendingDecksListProps extends React.ClassAttributes<TrendingDecksList> {
+	cardData?: CardData;
+	data?: TableData;
+}
+
+export default class TrendingDecksList extends React.Component<TrendingDecksListProps, void> {
+	render(): JSX.Element {
+		const decks: DeckObj[] = [];
+		const data = this.props.data.series.data;
+		Object.keys(data).forEach((key) => {
+			if (!data[key].length) {
+				return;
+			}
+			data[key].sort((a, b) => +b["popularity_delta"] - +a["popularity_delta"]);
+			const deck = data[key][0];
+			const cards = JSON.parse(deck["deck_list"]);
+			const deckList = cards.map((c) => {return {card: this.props.cardData.fromDbf(c[0]), count: c[1]}; });
+			decks.push({
+				cards: deckList,
+				deckId: +deck["deck_id"],
+				duration: +deck["avg_game_length_seconds"],
+				numGames: +deck["total_games"],
+				playerClass: key,
+				winrate: +deck["win_rate"],
+			});
+		});
+		decks.sort((a, b) => a.playerClass > b.playerClass ? 1 : -1);
+		return <DeckList decks={decks} pageSize={9} hideTopPager urlGameType={null}/>;
+	}
+}
--- hsreplaynet/static/scripts/src/pages/DeckSpotlight.tsx
+++ hsreplaynet/static/scripts/src/pages/DeckSpotlight.tsx
@@ -1,97 +1,33 @@
 import * as React from "react";
 import CardData from "../CardData";
-import DeckList from "../components/DeckList";
-import QueryManager from "../QueryManager";
-import {DeckObj, TableData, TableQueryData} from "../interfaces";
-import {toQueryString} from "../QueryParser"
-
-interface DeckSpotlightState {
-	deckData?: TableData;
-}
+import DataInjector from "../components/DataInjector";
+import TableLoading from "../components/loading/TableLoading";
+import TrendingDecksList from "../components/trending/TrendingDecksList";
+import DataManager from "../DataManager";
 
 interface DeckSpotlightProps extends React.ClassAttributes<DeckSpotlight> {
 	cardData: CardData;
 }
 
-export default class DeckSpotlight extends React.Component<DeckSpotlightProps, DeckSpotlightState> {
-	private readonly queryManager: QueryManager = new QueryManager();
-
-	constructor(props: DeckSpotlightProps, state: DeckSpotlightState) {
-		super(props, state);
-		this.state = {
-			deckData: "loading",
-		}
-		this.fetch();
-	}
+export default class DeckSpotlight extends React.Component<DeckSpotlightProps, void> {
+	private readonly dataManager: DataManager = new DataManager();
 
 	render(): JSX.Element {
-		const deckData = this.state.deckData;
-		let content = null;
-
-		if (!deckData || deckData === "loading" || !this.props.cardData) {
-			content = (
-				<div className="content-message">
-					<h2>Loading…</h2>
-				</div>
-			);
-		}
-		else if (deckData === "error") {
-			content = (
-				<div className="content-message">
-					<h2>Can't find decks :(</h2>
-					Please check back later.
-				</div>
-			);
-		}
-		else {
-			const decks = this.getDecks(deckData);
-			content = [
-				<h3>Here's a selection of decks which have been rising in popularity over the last 48 hours.</h3>,
-				<h3>Try them out to see what you think!</h3>,
-				<DeckList decks={decks} pageSize={9} hideTopPager urlGameType={null}/>
-			];
-		}
-
 		return (
 			<div id="deck-spotlight">
 				<h1>Trending Decks</h1>
-				{content}
+				<h3>Here's a selection of decks which have been rising in popularity over the last 48 hours.</h3>
+				<h3>Try them out to see what you think!</h3>
+				<DataInjector dataManager={this.dataManager} url="trending_decks_by_popularity">
+					<TableLoading cardData={this.props.cardData}>
+						<TrendingDecksList />
+					</TableLoading>
+				</DataInjector>
 				<section id="deck-db-link">
 					<h2>Can't find what you are looking for?</h2>
 					<a href="/decks/" className="promo-button">Check out all the decks!</a>
 				</section>
 			</div>
-		);
-	}
-	
-	getDecks(deckData: TableQueryData): DeckObj[] {
-		const decks: DeckObj[] = [];
-		const data = deckData.series.data;
-		Object.keys(data).forEach(key => {
-			if (!data[key].length) {
-				return;
-			}
-			data[key].sort((a, b) => +b["popularity_delta"] - +a["popularity_delta"]);
-			const deck = data[key][0];
-			const cards = JSON.parse(deck["deck_list"]);
-			const deckList = cards.map(c => {return {card: this.props.cardData.fromDbf(c[0]), count: c[1]}});
-			decks.push({
-				cards: deckList,
-				deckId: +deck["deck_id"],
-				duration: +deck["avg_game_length_seconds"],
-				numGames: +deck["total_games"],
-				playerClass: key,
-				winrate: +deck["win_rate"],
-			});
-		});
-		decks.sort((a, b) => a.playerClass > b.playerClass ? 1 : -1);
-		return decks;
-	}
-
-	fetch(): void {
-		this.queryManager.fetch(
-			"/analytics/query/trending_decks_by_popularity",
-			(data) => this.setState({deckData: data})
 		);
 	}
 }
