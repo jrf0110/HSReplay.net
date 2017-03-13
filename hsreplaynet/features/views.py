9151dc8685bb8fa63da8a6fc6eef1a78387a500a
Jerome Leclanche <jerome@leclan.ch>
2017-03-13 22:11:36 +0200

    Implement ?next support for feature redemptions

--- hsreplaynet/features/views.py
+++ hsreplaynet/features/views.py
@@ -1,7 +1,10 @@
 from django.contrib import messages
+from django.contrib.auth import REDIRECT_FIELD_NAME
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.forms import CharField, Form, TextInput, ValidationError
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
+from django.utils.http import is_safe_url
 from django.views.generic import FormView
 from .models import FeatureError, FeatureInvite
 
@@ -37,4 +40,10 @@ class FeatureInviteRedeemView(LoginRequiredMixin, FormView):
 			messages.error(self.request, "This code has expired.")
 		else:
 			messages.info(self.request, "Code successfully redeemed.")
+
+			next = self.request.GET.get(REDIRECT_FIELD_NAME)
+			# is_safe_url() will ensure we don't redirect to another domain
+			if next and is_safe_url(next):
+				return redirect(next)
+
 		return super().form_valid(form)
