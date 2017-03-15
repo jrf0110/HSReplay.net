2c49af3ea230d32f4b2661cd8c88e2af56c20efc
Jerome Leclanche <jerome@leclan.ch>
2017-03-15 04:26:47 +0200

    Move highlights page to profiles feature tag

--- hsreplaynet/profiles/views.py
+++ hsreplaynet/profiles/views.py
@@ -4,6 +4,6 @@ from django.views.generic import TemplateView
 from hsreplaynet.features.decorators import view_requires_feature_access
 
 
-@method_decorator(view_requires_feature_access("carddb"), name="dispatch")
+@method_decorator(view_requires_feature_access("profiles"), name="dispatch")
 class HighlightsView(LoginRequiredMixin, TemplateView):
 	template_name = "profiles/highlights.html"
