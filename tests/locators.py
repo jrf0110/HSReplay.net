9953628e1b95168d9b8330f5a4b89949a9ccfecc
Andrew Wilson <andrewmoorewilson@gmail.com>
2017-03-15 22:52:14 -0400

    Update locators for selenium

--- tests/locators.py
+++ tests/locators.py
@@ -2,19 +2,19 @@ from selenium.webdriver.common.by import By
 
 
 class SitewideLocators(object):
-	ACCOUNT_DETAILS = (By.XPATH, '//*[@id="navbar-collapse"]/ul[2]/li[3]/a')
+	ACCOUNT_DETAILS = (By.ID, 'you')
 
 
 class AccountDetailLocators(SitewideLocators):
-	SIGN_OUT = (By.XPATH, '/html/body/nav/div/ul[2]/li/a')
-	CONFIRM_SIGN_OUT = (By.XPATH, '/html/body/div/form/button')
+	SIGN_OUT = (By.ID, 'sign-out')
+	CONFIRM_SIGN_OUT = (By.XPATH, '/html/body/div[2]/form/button')
 
 
 class DeckDatabaseLocators(SitewideLocators):
 	DECK_LIST_ROWS = (By.XPATH, '//*[@id="deck-discover-container"]/div/div[2]/div/ul/li')
 	INCLUDED_CARDS_SEARCH_BOX = (By.XPATH, '//*[@id="deck-discover-infobox"]/div[3]/input')
 	DECK_ROW_DECK_NAME = (By.CLASS_NAME, "deck-name")
-	PREMIUM_MORE_INFO = (By.XPATH, '//*[@id="deck-discover-infobox"]/div[5]/div[2]/span')
+	PREMIUM_MORE_INFO = (By.XPATH, '//*[@id="deck-discover-infobox"]/div[6]/div[2]/h4/span')
 	PREMIUM_SIGNUP_BUTTON = (By.XPATH, '//*[@id="premium-modal"]/div/form/button/span')
 	RANK_RANGE_FILTER = (By.XPATH, '//*[@id="deck-discover-infobox"]/div[6]/ul/li')
 
