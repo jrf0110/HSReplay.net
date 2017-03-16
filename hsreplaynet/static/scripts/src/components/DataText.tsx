f581edddea365d2f48279f09dfe2c6f67052b3ab
Benedict Etzel <benedict@hearthsim.net>
2017-03-16 14:37:00 +0100

    DataText: Add p prop

--- hsreplaynet/static/scripts/src/components/DataText.tsx
+++ hsreplaynet/static/scripts/src/components/DataText.tsx
@@ -2,10 +2,14 @@ import * as React from "react";
 
 interface DataTextProps extends React.ClassAttributes<DataText> {
 	data?: any;
+	p?: boolean;
 }
 
 export default class DataText extends React.Component<DataTextProps, void> {
 	render(): JSX.Element {
+		if(typeof this.props.p !== "undefined" && !this.props.p) {
+			return <span>{this.props.data}</span>;
+		}
 		return <p>{this.props.data}</p>;
 	}
 }
