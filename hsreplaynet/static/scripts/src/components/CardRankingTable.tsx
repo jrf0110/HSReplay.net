32aa188c0a16f1fe4981beeb11cfcfba3457c8dc
Alexander Zeier <alex@hearthsim.net>
2017-03-16 19:49:45 +0100

    Clean up CardRankingTable
    Removed unnecessary cardData guard

--- hsreplaynet/static/scripts/src/components/CardRankingTable.tsx
+++ hsreplaynet/static/scripts/src/components/CardRankingTable.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import CardData from "../CardData";
 import {TableData} from "../interfaces";
-import {getQueryMapFromLocation} from "../QueryParser";
 import CardRankingTableRow from "./CardRankingTableRow";
 
 interface CardRankingTableProps extends React.ClassAttributes<CardRankingTable> {
@@ -16,32 +15,29 @@ interface CardRankingTableProps extends React.ClassAttributes<CardRankingTable> 
 export default class CardRankingTable extends React.Component<CardRankingTableProps, any> {
 	render(): JSX.Element {
 		const cardRows = [];
-		if (this.props.cardData) {
-			const tableRows = this.props.data.series.data[this.props.dataKey];
-			tableRows.sort((a, b) => +b["popularity"] - +a["popularity"]);
-			tableRows.slice(0, this.props.numRows).forEach((row, index) => {
-				const isFace = +row["dbf_id"] === -1;
-				const card = this.props.cardData.fromDbf(isFace ? 39770 : row["dbf_id"]);
-				cardRows.push(
-					<CardRankingTableRow
-						card={card}
-						popularity={+row["popularity"]}
-						rank={index + 1}
-						clickable={this.props.clickable}
-						urlGameType={this.props.urlGameType}
-						customCardText={isFace ? "Opponent Hero" : undefined}
-					/>,
-				);
-			});
-		}
+		const tableRows = this.props.data.series.data[this.props.dataKey];
+		tableRows.sort((a, b) => +b.popularity - +a.popularity);
+		tableRows.slice(0, this.props.numRows).forEach((row, index) => {
+			const isFace = +row.dbf_id === -1;
+			const card = this.props.cardData.fromDbf(isFace ? 39770 : row.dbf_id);
+			cardRows.push(
+				<CardRankingTableRow
+					card={card}
+					popularity={+row.popularity}
+					rank={index + 1}
+					clickable={this.props.clickable}
+					urlGameType={this.props.urlGameType}
+					customCardText={isFace ? "Opponent Hero" : undefined}
+				/>,
+			);
+		});
 		return (
 			<table className="table table-striped">
 				<thead>
 				<tr>
 					<th>Rank</th>
 					<th>Card</th>
-					<th className="hidden-xs">Popularity</th>
-					<th className="visible-xs">Pop.</th>
+					<th>Popularity</th>
 				</tr>
 				</thead>
 				<tbody>
