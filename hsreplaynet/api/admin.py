73ca5d038b00c220c3dbc624264ad8448007c09c
Jerome Leclanche <jerome@leclan.ch>
2017-03-17 15:41:30 +0200

    Add token count to AuthTokenAdmin

--- hsreplaynet/api/admin.py
+++ hsreplaynet/api/admin.py
@@ -18,10 +18,13 @@ class AuthTokenAdmin(admin.ModelAdmin):
 
 @admin.register(APIKey)
 class APIKeyAdmin(admin.ModelAdmin):
-	list_display = ("__str__", "email", "website", "api_key", "enabled")
+	list_display = ("__str__", "email", "website", "api_key", "enabled", "token_count")
 	search_fields = ("full_name", "email", "website")
 	list_filter = ("enabled", )
 	exclude = ("tokens", )
 	readonly_fields = ("api_key", )
 	show_full_result_count = False
 	paginator = EstimatedCountPaginator
+
+	def token_count(self, obj):
+		return obj.tokens.count()
