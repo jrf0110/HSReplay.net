8edc72a6623e9ba10cdae042536561584f775413
Benedict Etzel <benedict@hearthsim.net>
2017-03-22 02:07:19 +0100

    Upgrade Raven to 3.13.0

--- scripts/get_vendor_static.sh
+++ scripts/get_vendor_static.sh
@@ -58,7 +58,7 @@ wget --no-verbose "$SOURCE" -O "$OUTFILE"
 # Raven
 
 PKGNAME="raven"
-VERSION="3.12.1"
+VERSION="3.13.0"
 SOURCE="https://cdn.ravenjs.com/$VERSION/$PKGNAME.min.js"
 OUTFILE="$STATICDIR/$PKGNAME.min.js"
 
