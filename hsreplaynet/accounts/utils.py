151ba690379da8286ab1c6b0a43a1e69a12d38d8
Jerome Leclanche <jerome@leclan.ch>
2017-03-22 13:18:21 +0200

    Implement StripeCustomer merging

--- hsreplaynet/accounts/utils.py
+++ hsreplaynet/accounts/utils.py
@@ -1,3 +1,4 @@
+from djstripe.models import Customer
 from hsreplaynet.utils import log
 
 
@@ -33,3 +34,27 @@ def merge_users(base_user, user):
 
 	# Webhooks
 	do_queryset(user.webhooks)
+
+	# Stripe customers
+
+	# Always delete livemode customers
+	Customer.objects.filter(livemode=False, subscriber=user).delete()
+
+	customers = Customer.objects.filter(livemode=True)
+	user_has_customer = customers.filter(subscriber=user).exists()
+	base_user_has_customer = customers.filter(subscriber=base_user).exists()
+
+	if user_has_customer and base_user_has_customer:
+		cus1 = customers.get(subscriber=user)
+		cus2 = customers.get(subscriber=base_user)
+		log.warn("Found customers for both users: %r and %r. Merge manually!", cus1, cus2)
+	elif user_has_customer:
+		cus = customers.get(subscriber=user)
+		log.info("Moving Customer %r from user %r to user %r", cus, user, base_user)
+		cus.subscriber = base_user
+		cus.metadata["django_account"] = str(base_user.pk)
+		cus.save()
+	elif base_user_has_customer:
+		log.info("Found Stripe customer on %r, nothing to do.", base_user)
+	else:
+		log.info("No Stripe customers to merge.")
