2bb4aaa2c03d28884e9daa7316f8e32ef8a30434
Jerome Leclanche <jerome@leclan.ch>
2017-03-26 07:01:36 +0300

    Use Customer.active_subscriptions property in is_premium()

--- hsreplaynet/accounts/models.py
+++ hsreplaynet/accounts/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import AbstractUser
 from django.contrib.postgres.fields import JSONField
 from django.db import models
 from django.urls import reverse
-from django.utils import timezone
 from django.utils.functional import cached_property
 from django_intenum import IntEnumField
 from hsreplaynet.games.models import Visibility
@@ -88,10 +87,8 @@ class User(AbstractUser):
 			# Override to false if we don't have a Stripe secret key to avoid unnecessary errors.
 			return False
 
-		now = timezone.now()
 		customer = self.stripe_customer
-		subscriptions = customer.subscriptions.filter(status="active", current_period_end__gt=now)
-		return subscriptions.count() > 0
+		return customer.active_subscriptions.count() > 0
 
 	def delete_replays(self):
 		self.replays.update(is_deleted=True)
