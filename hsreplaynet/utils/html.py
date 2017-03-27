64d3e2a3edb9c3dc2aa3f1626c3d756e82a53421
Jerome Leclanche <jerome@leclan.ch>
2017-03-27 15:54:37 +0300

    Add support for stylesheets in HTMLHead

--- hsreplaynet/utils/html.py
+++ hsreplaynet/utils/html.py
@@ -1,3 +1,4 @@
+from django.contrib.staticfiles.templatetags.staticfiles import static
 from django.utils.html import escape
 
 
@@ -26,9 +27,12 @@ class HTMLTag:
 class HTMLHead:
 	def __init__(self, request):
 		self._meta_tags = []
-		self.canonical_url = ""
+		self._link_tags = []
 		self.request = request
 		self.charset = "utf-8"
+		self.base_title = ""
+		self.title = ""
+		self.canonical_url = ""
 
 	def __str__(self):
 		return "".join(str(tag) for tag in self.get_tags())
@@ -40,6 +44,7 @@ class HTMLHead:
 			tags.append(HTMLTag("meta", attrs={"charset": "utf-8"}))
 
 		tags += self._meta_tags
+		tags += self._link_tags
 
 		if self.canonical_url:
 			tags.append(HTMLTag("meta", attrs={"property": "og:url", "content": self.canonical_url}))
@@ -47,9 +52,18 @@ class HTMLHead:
 
 		return tags
 
+	def add_link(self, **attrs):
+		self._link_tags.append(HTMLTag("link", attrs=attrs))
+
 	def add_meta(self, *tags):
 		for attrs in tags:
 			self._meta_tags.append(HTMLTag("meta", attrs=attrs))
+
+	def add_stylesheets(self, *urls):
+		for url in urls:
+			if not url.startswith(("http:", "https:")):
+				url = static(url)
+			self.add_link(rel="stylesheet", type="text/css", href=url)
 
 	def set_canonical_url(self, url):
 		self.canonical_url = self.request.build_absolute_uri(url)
