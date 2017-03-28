c34e9a965769294e52de1298d5abfbcb839fcfc7
Benedict Etzel <benedict@hearthsim.net>
2017-03-28 20:27:52 +0200

    Clean up ClassIcon

--- hsreplaynet/static/scripts/src/components/ClassIcon.tsx
+++ hsreplaynet/static/scripts/src/components/ClassIcon.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import Tooltip from "./Tooltip";
-import { toTitleCase } from "../helpers";
+import {toTitleCase} from "../helpers";
 
 export interface ClassIconProps {
 	heroClassName: string;
@@ -9,19 +9,36 @@ export interface ClassIconProps {
 }
 
 export default class ClassIcon extends React.Component<ClassIconProps, any> {
-	private readonly classes = ["druid", "hunter", "mage", "paladin", "priest", "rogue", "shaman", "warlock", "warrior"];
+	private readonly classes = [
+		"druid",
+		"hunter",
+		"mage",
+		"paladin",
+		"priest",
+		"rogue",
+		"shaman",
+		"warlock",
+		"warrior",
+	];
 
 	render(): JSX.Element {
 		const basePath = STATIC_URL + "images/" + (this.props.small ? "64x/" : "");
-		const heroClassName = this.props.heroClassName.toLowerCase();
+		const heroClassName = toTitleCase(this.props.heroClassName);
+		const fileName = this.props.heroClassName.toLowerCase();
 		let image = null;
-		if (this.classes.indexOf(heroClassName) === -1) {
-			image = <img src={basePath + "mode-icons/mode_ai.png"} className="class-icon" alt={heroClassName}/>;
+		if (this.classes.indexOf(fileName) === -1) {
+			image = (
+				<img
+					src={basePath + "mode-icons/mode_ai.png"}
+					className="class-icon"
+					alt={heroClassName}
+				/>
+			);
 		}
 		else {
 			image = (
 				<img
-					src={basePath + "class-icons/" + heroClassName + ".png"}
+					src={basePath + "class-icons/" + fileName + ".png"}
 					className="class-icon"
 					alt={heroClassName}
 				/>
@@ -29,7 +46,10 @@ export default class ClassIcon extends React.Component<ClassIconProps, any> {
 		}
 		if (this.props.tooltip) {
 			image = (
-				<Tooltip content={toTitleCase(this.props.heroClassName)} simple>
+				<Tooltip
+					content={toTitleCase(this.props.heroClassName)}
+					simple
+				>
 					{image}
 				</Tooltip>
 			);
