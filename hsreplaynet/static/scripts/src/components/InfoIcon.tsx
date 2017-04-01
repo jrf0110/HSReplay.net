5d7cac41cfd4287e93c2b12ea79af522bb348753
Benedict Etzel <benedict@hearthsim.net>
2017-04-01 22:29:11 +0200

    InfoIcon: Optimize for infobox

--- hsreplaynet/static/scripts/src/components/InfoIcon.tsx
+++ hsreplaynet/static/scripts/src/components/InfoIcon.tsx
@@ -4,12 +4,17 @@ import Tooltip from "./Tooltip";
 interface InfoIconProps extends React.ClassAttributes<InfoIcon> {
 	content?: string | JSX.Element;
 	header?: string;
+	className?: string;
 }
 
 export default class InfoIcon extends React.Component<InfoIconProps, void> {
 	render(): JSX.Element {
 		return (
-			<Tooltip className="info-icon" header={this.props.header} content={this.props.content}>
+			<Tooltip
+				className={"info-icon" + (this.props.className ? " " + this.props.className : "")}
+				header={this.props.header}
+				content={this.props.content}
+			>
 				<span className="glyphicon glyphicon-question-sign"/>
 			</Tooltip>
 		);
