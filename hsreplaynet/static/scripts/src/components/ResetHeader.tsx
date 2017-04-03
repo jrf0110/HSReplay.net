d765d604a72e1503bdb9744c320d3b735324aee6
Benedict Etzel <benedict@hearthsim.net>
2017-04-03 16:40:37 +0200

    ResetHeader: Improve keyboard accessibility

--- hsreplaynet/static/scripts/src/components/ResetHeader.tsx
+++ hsreplaynet/static/scripts/src/components/ResetHeader.tsx
@@ -12,7 +12,20 @@ export default class ResetHeader extends React.Component<ResetHeaderProps, void>
 			classNames.push("btn btn-danger btn-full")
 		}
 		return (
-			<h1 className={classNames.join(" ")} onClick={() => this.props.onReset()}>
+			<h1
+				className={classNames.join(" ")}
+				onClick={() => this.props.onReset()}
+				onKeyPress={(event) => {
+					if(event.which !== 13) {
+						return;
+					}
+					if(event.target) {
+						(event.target as any).blur();
+					}
+					this.props.onReset();
+				}}
+				tabIndex={this.props.showReset ? 0 : -1}
+			>
 				{this.props.showReset ? "Reset all filters" : this.props.children}
 			</h1>
 		);
