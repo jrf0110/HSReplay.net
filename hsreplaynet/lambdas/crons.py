81ad5bfa88a57e1f21f0a3289192b2706c3b0351
Andrew Wilson <andrewmoorewilson@gmail.com>
2017-04-14 21:24:50 -0400

    Enable Redshift ETL to connect to Redis

--- hsreplaynet/lambdas/crons.py
+++ hsreplaynet/lambdas/crons.py
@@ -13,7 +13,11 @@ from hsreplaynet.utils import aws, instrumentation, log
 from hsreplaynet.utils.influx import influx_metric
 
 
-@instrumentation.lambda_handler(cpu_seconds=300, tracing=False)
+@instrumentation.lambda_handler(
+	cpu_seconds=300,
+	requires_vpc_access=True,
+	tracing=False
+)
 def do_redshift_etl_maintenance(event, context):
 	"""A periodic job to orchestrate Redshift ETL Maintenance"""
 	RedshiftStagingTrack.objects.do_maintenance()
