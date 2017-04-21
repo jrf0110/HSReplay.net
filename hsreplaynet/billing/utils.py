7077d9bcb32bc569645313ebf1b595c7b32b9e8a
Andrew Wilson <andrewmoorewilson@gmail.com>
2017-04-21 10:52:32 -0400

    Move subscription created signal to get initialized on app start

--- hsreplaynet/billing/utils.py
+++ hsreplaynet/billing/utils.py
@@ -1,31 +1,4 @@
-from django.dispatch import receiver
-from djstripe import signals
-from hsreplaynet.analytics.processing import (
-	PremiumUserCacheWarmingContext, warm_redshift_cache_for_user_context
-)
-from hsreplaynet.utils import log
-from hsreplaynet.utils.influx import influx_metric
-
-
-@receiver(signals.WEBHOOK_SIGNALS["customer.subscription.created"])
-def on_premium_purchased(sender, event, **kwargs):
-	if event.customer and event.customer.subscriber:
-		user = event.customer.subscriber
-		log.info("Received premium purchased signal for user: %s" % user.username)
-		log.info("Scheduling personalized stats for immediate cache warming.")
-		context = PremiumUserCacheWarmingContext.from_user(user)
-
-		fields = {
-			"count": 1,
-			"user": user.username
-		}
-
-		influx_metric(
-			"premium_purchase_cache_warm_event",
-			fields,
-		)
-
-		warm_redshift_cache_for_user_context(context)
+from hsreplaynet.analytics.processing import PremiumUserCacheWarmingContext
 
 
 def get_premium_cache_warming_contexts_from_subscriptions():
