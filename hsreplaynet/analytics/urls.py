b8dcb290951c8500cca7cbd0e5d4223f54f8d516
Jerome Leclanche <jerome@leclan.ch>
2017-04-22 17:11:55 +0300

    Allow trailing slashes in analytics urls

--- hsreplaynet/analytics/urls.py
+++ hsreplaynet/analytics/urls.py
@@ -4,26 +4,26 @@ from . import views
 
 urlpatterns = [
 	url(
-		r"^query/(?P<name>\w+)$", views.fetch_query_results,
+		r"^query/(?P<name>\w+)/$", views.fetch_query_results,
 		name="analytics_fetch_query_results"
 	),
 	url(
-		r"^local/query/(?P<name>\w+)$", views.fetch_local_query_results,
+		r"^local/query/(?P<name>\w+)/$", views.fetch_local_query_results,
 		name="analytics_fetch_local_query_results"
 	),
 	url(
-		r"^evict/(?P<name>\w+)$", views.evict_query_from_cache,
+		r"^evict/(?P<name>\w+)/$", views.evict_query_from_cache,
 		name="analytics_evict_from_cache"
 	),
 	url(
-		r"^release/semaphore/(?P<name>\w+)$", views.release_semaphore,
+		r"^release/semaphore/(?P<name>\w+)/$", views.release_semaphore,
 		name="analytics_release_semaphore"
 	),
 ]
 
 api_urlpatterns = [
 	url(
-		r"^v1/analytics/query/(?P<name>\w+)$", views.fetch_query_results,
+		r"^v1/analytics/query/(?P<name>\w+)/$", views.fetch_query_results,
 		name="analytics_api_fetch_query_results"
 	),
 ]
