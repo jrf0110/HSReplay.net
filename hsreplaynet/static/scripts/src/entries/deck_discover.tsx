37e4cbe8ba45b60a1cbd856055634c0eaba2b3f5
Benedict Etzel <benedict@hearthsim.net>
2017-05-17 02:32:57 +0200

    Make DeckDiscover translateable

--- hsreplaynet/static/scripts/src/entries/deck_discover.tsx
+++ hsreplaynet/static/scripts/src/entries/deck_discover.tsx
@@ -4,32 +4,36 @@ import CardData from "../CardData";
 import DeckDiscover from "../pages/DeckDiscover";
 import UserData from "../UserData";
 import Fragments from "../components/Fragments";
+import {I18nextProvider} from "react-i18next";
+import i18n from "../i18n";
 
 const container = document.getElementById("deck-discover-container");
 const user = new UserData();
 
 const render = (cardData: CardData) => {
 	ReactDOM.render(
-		<Fragments
-			defaults={{
-				account: "",
-				excludedCards: [],
-				gameType: "RANKED_STANDARD",
-				includedCards: [],
-				includedSet: "ALL",
-				playerClasses: [],
-				opponentClasses: [],
-				rankRange: "ALL",
-				region: "ALL",
-				timeRange: "LAST_30_DAYS",
-			}}
-			immutable={!user.isPremium() ? ["account", "timeRange", "opponentClass", "rankRange"] : null}
-		>
-			<DeckDiscover
-				cardData={cardData}
-				user={user}
-			/>
-		</Fragments>,
+		<I18nextProvider i18n={i18n}>
+			<Fragments
+				defaults={{
+					account: "",
+					excludedCards: [],
+					gameType: "RANKED_STANDARD",
+					includedCards: [],
+					includedSet: "ALL",
+					playerClasses: [],
+					opponentClasses: [],
+					rankRange: "ALL",
+					region: "ALL",
+					timeRange: "LAST_30_DAYS",
+				}}
+				immutable={!user.isPremium() ? ["account", "timeRange", "opponentClass", "rankRange"] : null}
+			>
+				<DeckDiscover
+					cardData={cardData}
+					user={user}
+				/>
+			</Fragments>
+		</I18nextProvider>,
 		container,
 	);
 };
