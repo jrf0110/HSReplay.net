1a46dbf81a562a5321a761981846a34fbdac8b95
Benedict Etzel <benedict@hearthsim.net>
2017-06-02 23:22:25 +0200

    Add PropRemapper component

---
+++ hsreplaynet/static/scripts/src/components/utils/PropRemapper.tsx
@@ -0,0 +1,33 @@
+import * as React from "react";
+
+interface PropMap {
+	[prop: string]: string;
+}
+
+interface PropRemapperProps extends React.ClassAttributes<PropRemapper> {
+	map?: PropMap;
+}
+
+export default class PropRemapper extends React.Component<PropRemapperProps, void> {
+	render(): JSX.Element {
+		const child = this.props.children;
+		if (!child) {
+			return;
+		}
+		const props = {};
+		Object.keys(this.props).forEach((key: string) => {
+			if (key === "map" || key === "children") {
+				return;
+			}
+			const value = this.props[key];
+			if (typeof this.props.map[key] === "string") {
+				props[this.props.map[key]] = value;
+			}
+			else {
+				props[key] = value;
+			}
+		});
+
+		return React.cloneElement(child as any, props);
+	}
+}
