b9075a68201d5bec87ebf164deea4cd3bf7accc9
Benedict Etzel <benedict@hearthsim.net>
2017-06-03 13:41:02 +0200

    Analytics API: Return 404 if deck digest does not exist
    Fixes HSREPLAYNET-12G.

--- hsreplaynet/analytics/views.py
+++ hsreplaynet/analytics/views.py
@@ -50,8 +50,11 @@ def _get_query_and_params(request, name):
 	supplied_params = request.GET.dict()
 	if "deck_id" in supplied_params and not supplied_params["deck_id"].isdigit():
 		# We got sent a shortid, so we need to translate it into a deck_id int
-		deck = Deck.objects.get_by_shortid(supplied_params["deck_id"])
-		supplied_params["deck_id"] = str(deck.id)
+		try:
+			deck = Deck.objects.get_by_shortid(supplied_params["deck_id"])
+			supplied_params["deck_id"] = str(deck.id)
+		except Deck.DoesNotExist:
+			raise Http404("Deck does not exist")
 
 	if query.is_personalized:
 		if request.user and request.user.is_authenticated:
