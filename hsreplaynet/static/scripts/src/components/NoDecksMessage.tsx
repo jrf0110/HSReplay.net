6e76286c44fcb5efff612bd5cef8df1f4183a9dc
Jerome Leclanche <jerome@leclan.ch>
2017-06-26 13:39:30 +0300

    Reword NoDecksMessage

--- hsreplaynet/static/scripts/src/components/NoDecksMessage.tsx
+++ hsreplaynet/static/scripts/src/components/NoDecksMessage.tsx
@@ -11,7 +11,7 @@ export default class NoDecksMessage extends React.Component<NoDecksMessageProps,
 				<h3>We can fix that!</h3>
 				<p>Step 1: If you haven't already, <a href="/downloads/">download Hearthstone Deck Tracker</a></p>
 				<p>Step 2: Play some games with it and your decks will show up here!</p><br />
-				<p><i>Protip: We have a "Copy deck to HDT" button on every deck page!</i></p><br />
+				<p><em>Protip: There is a "Copy deck to Hearthstone" button on every deck page!</em></p><br />
 				{this.props.children}
 			</div>
 		);
