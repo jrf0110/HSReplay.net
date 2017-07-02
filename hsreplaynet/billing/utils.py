c37030558d7b1e65ade7a84f631974418c3aa9ce
Andrew Wilson <andrewmoorewilson@gmail.com>
2017-07-02 19:31:33 -0400

    Add logging to Subscription receiver

--- hsreplaynet/billing/utils.py
+++ hsreplaynet/billing/utils.py
@@ -4,6 +4,7 @@ from djstripe.models import Subscription
 from hsreplaynet.analytics.processing import (
 	PremiumUserCacheWarmingContext, synchronize_redshift_premium_accounts_for_user
 )
+from hsreplaynet.utils import log
 
 
 def get_premium_cache_warming_contexts_from_subscriptions():
@@ -18,6 +19,14 @@ def get_premium_cache_warming_contexts_from_subscriptions():
 
 @receiver(models.signals.post_save, sender=Subscription)
 def sync_subscription_to_redshift(sender, instance, **kwargs):
-	if instance.customer.subscriber:
-		user = instance.customer.subscriber
-		synchronize_redshift_premium_accounts_for_user(user)
+	log.info("Receiver fired for subscription: %s" % str(instance))
+	if instance.customer:
+		log.info("Customer: %s" % str(instance.customer))
+		if instance.customer.subscriber:
+			user = instance.customer.subscriber
+			log.info("User ID for subscription is: %s" % str(user.id))
+			synchronize_redshift_premium_accounts_for_user(user)
+		else:
+			log.info("No user attached to subscription.customer.")
+	else:
+		log.info("No customer set on object")
