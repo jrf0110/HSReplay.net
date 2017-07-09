743189e49e0432956c0106318883c33496521f72
Alexander Zeier <alex@hearthsim.net>
2017-07-09 14:47:42 +0200

    Add class icons with integrated favorite icon on row headers

--- hsreplaynet/static/scripts/src/components/metaoverview/RowHeader.tsx
+++ hsreplaynet/static/scripts/src/components/metaoverview/RowHeader.tsx
@@ -12,20 +12,28 @@ interface RowHeaderState {
 
 export default class RowHeader extends React.Component<RowHeaderProps, RowHeaderState> {
 	render() {
-		const favIconClasses = ["glyphicon glyphicon-star"];
+		let activeFavIcon = null;
+		const favIconClasses = ["glyphicon glyphicon-star favorite-toggle"];
 		if (this.props.isFavorite) {
 			favIconClasses.push("favorite");
+			activeFavIcon = <span className="glyphicon glyphicon-star active-favorite"/>;
 		}
 
 		return (
 			<th
 				className="matchup-row-header"
+				onClick={() => this.props.onFavoriteClick()}
 			>
+				<div className="class-icon-wrapper">
+					<img
+						className="class-icon"
+						src={`${STATIC_URL}images/64x/class-icons/${this.props.archetypeData.playerClass.toLowerCase()}.png`}
+					/>
+					<span className={favIconClasses.join(" ")}/>
+					{activeFavIcon}
+				</div>
 				{this.props.archetypeData.name}
-				<span
-					className={favIconClasses.join(" ")}
-					onClick={() => this.props.onFavoriteClick()}
-				/>
+				<span className="glyphicon glyphicon-new-window"/>
 			</th>
 		);
 	}
