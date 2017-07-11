bd81663d80406416e43d9083dc4d7003a2788ea5
Alexander Zeier <alex@hearthsim.net>
2017-07-11 13:11:22 +0200

    Add tooltips on remaining matrix cells

--- hsreplaynet/static/scripts/src/components/metaoverview/MatchupCell.tsx
+++ hsreplaynet/static/scripts/src/components/metaoverview/MatchupCell.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { MatchupData } from "../../interfaces";
-import { toDynamicFixed, getColorString } from "../../helpers";
+import { getColorString, toDynamicFixed } from "../../helpers";
 import {Colors} from "../../Colors";
 import Tooltip from "../Tooltip";
 
@@ -20,23 +20,41 @@ export default class MatchupCell extends React.Component<MatchupCellProps, Match
 		const winrate = this.props.matchupData.winrate || 0;
 		const classNames = [];
 
-		if(this.props.matchupData.friendlyId === this.props.matchupData.opponentId) {
+		if (this.props.matchupData.friendlyId === this.props.matchupData.opponentId) {
 			// mirror match
 			label = <Tooltip content="Mirror&nbsp;matchup" simple>⟋</Tooltip>;
 			backgroundColor = "rgb(200,200,200)";
 		}
-		else if(this.props.matchupData.totalGames > 100) {
+		else if (this.props.matchupData.totalGames > 100) {
 			// actual matchup
 			backgroundColor = getColorString(Colors.HSREPLAY, 50, winrate / 100, false);
-			label = `${toDynamicFixed(winrate, 2)}%`;
+			label = (
+				<Tooltip
+					simple
+					content={(
+						<table>
+							<tr>
+								<th>Winrate:</th>
+								<td>{toDynamicFixed(winrate, 2)}%</td>
+							</tr>
+							<tr>
+								<th>Games:</th>
+								<td>{this.props.matchupData.totalGames}</td>
+							</tr>
+						</table>
+					)}
+				>
+					{`${toDynamicFixed(winrate, 2)}%`}
+				</Tooltip>
+			);
 		}
 		else {
 			// not enough data
-			label = "~";
+			label = <Tooltip content="Not enough games" simple>~</Tooltip>;
 			backgroundColor = "rgb(200,200,200)";
 		}
 
-		if(this.props.isIgnored) {
+		if (this.props.isIgnored) {
 			classNames.push("ignored");
 		}
 
