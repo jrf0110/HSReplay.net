c798250f279cb9408a91e93d0b07a05717b3892d
Benedict Etzel <developer@beheh.de>
2017-07-11 19:37:15 +0200

    Color RowFooter cells

--- hsreplaynet/static/scripts/src/components/metaoverview/RowFooter.tsx
+++ hsreplaynet/static/scripts/src/components/metaoverview/RowFooter.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 import {ArchetypeData} from "../../interfaces";
 import Bar, {BarDirection} from "./Bar";
+import {getColorString} from "../../helpers";
+import {Colors} from "../../Colors";
 
 interface RowFooterProps extends React.ClassAttributes<RowFooter> {
 	archetypeData?: ArchetypeData;
@@ -11,14 +13,18 @@ interface RowFooterState {
 
 export default class RowFooter extends React.Component<RowFooterProps, RowFooterState> {
 	render() {
+		const style = {
+			backgroundColor: "transparent",
+		};
+
+		const winrate = this.props.archetypeData.effectiveWinrate;
+		const color = getColorString(Colors.HSREPLAY, 50, winrate / 100, false);
+
+		style.backgroundColor = color;
+
 		return (
-			<td className="row-footer">
-				<Bar
-					total={100}
-					value={this.props.archetypeData.effectiveWinrate}
-					direction={BarDirection.HORIZONTAL}
-					label={`${this.props.archetypeData.effectiveWinrate}%`}
-				/>
+			<td className="row-footer" style={style}>
+				{`${winrate}%`}
 			</td>
 		);
 	}
