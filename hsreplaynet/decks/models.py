b4a4ef8105473f1eaf66831c39e9598a7d5880c1
Jerome Leclanche <jerome@leclan.ch>
2017-07-11 12:27:34 +0300

    Make classify_deck() a pure function

--- hsreplaynet/decks/models.py
+++ hsreplaynet/decks/models.py
@@ -63,8 +63,11 @@ class DeckManager(models.Manager):
 
 	def classify_deck_with_archetype(self, deck, player_class, game_format):
 		distance_cutoff = settings.ARCHETYPE_MINIMUM_SIGNATURE_MATCH_CUTOFF_DISTANCE
+		available_archetypes = list(Archetype.objects.filter(player_class=player_class))
+		signature_weights = self._fetch_signature_weights(available_archetypes, game_format)
+		card_counts = {i.card_id: i.count for i in deck.includes.all()}
 		archetype = Archetype.objects.classify_deck(
-			deck, player_class, game_format, distance_cutoff
+			card_counts, available_archetypes, signature_weights, distance_cutoff
 		)
 		if archetype:
 			deck.archetype = archetype
@@ -263,20 +266,14 @@ class ArchetypeManager(models.Manager):
 				result[record["archetype_id"]][record["card_id"]] = record["weight"]
 			return result
 
-	def classify_deck(self, deck, player_class, game_format, distance_cutoff):
+	def classify_deck(self, deck, available_archetypes, signature_weights, distance_cutoff):
 		distances = []
-		archetypes_for_class = list(Archetype.objects.filter(player_class=player_class))
-		if not archetypes_for_class:
-			return
-
-		signature_weights = self._fetch_signature_weights(archetypes_for_class, game_format)
-		card_counts = {i.card_id: i.count for i in deck.includes.all()}
-		for archetype in archetypes_for_class:
+		for archetype in available_archetypes:
 			distance = 0
 			if archetype.id in signature_weights:
 				for card_id, weight in signature_weights[archetype.id].items():
-					if card_id in card_counts:
-						distance += (card_counts[card_id] * weight)
+					if card_id in deck:
+						distance += (deck[card_id] * weight)
 
 			if distance and distance >= distance_cutoff:
 				distances.append((archetype, distance))
