a8265f43570f7e3d48f0b496a71791be547082fc
Jerome Leclanche <jerome@leclan.ch>
2017-07-26 12:52:36 +0300

    Replace reap_upload_events database procedure with Python logic

--- hsreplaynet/uploads/management/commands/clear_old_upload_data.py
+++ hsreplaynet/uploads/management/commands/clear_old_upload_data.py
@@ -1,40 +1,17 @@
 from datetime import timedelta
 from django.core.management.base import BaseCommand
-from django.db import connections
 from django.utils.timezone import now
-from hsreplaynet.uploads.models import Descriptor
-from hsreplaynet.utils.influx import influx_metric
+from hsreplaynet.uploads.models import Descriptor, UploadEvent
 
 
 class Command(BaseCommand):
-	help = "Clear old upload events"
-	successful_cutoff_days = 5
-	unsuccessful_cutoff_days = 60
-	database = "uploads"
+	help = "Clear old upload events and descriptors"
 
 	def handle(self, *args, **options):
-		cursor = connections[self.database].cursor()
-		ts = now()
-		args = (
-			ts.year, ts.month, ts.day, ts.hour,
-			self.successful_cutoff_days, self.unsuccessful_cutoff_days
-		)
+		cutoff = now() - timedelta(days=15)
 
-		cursor.callproc("reap_upload_events", args)
-		result_row = cursor.fetchone()
-		successful_reaped = result_row[0]
-		unsuccessful_reaped = result_row[1]
-		cursor.close()
+		deleted_uploadevents = UploadEvent.objects.filter(created__lt=cutoff).delete()
+		self.stdout.write("Deleted %i successful upload events" % (deleted_uploadevents[0]))
 
-		influx_metric("upload_events_reaped", fields={
-			"successful_reaped": successful_reaped,
-			"unsuccessful_reaped": unsuccessful_reaped
-		})
-
-		self.stdout.write("Deleted %i successful upload events" % (successful_reaped))
-		self.stdout.write("Deleted %i unsuccessful upload events" % (unsuccessful_reaped))
-
-		descriptors = Descriptor.objects.filter(created__lt=ts - timedelta(days=14))
-		deleted_descriptors = descriptors.delete()
-
-		self.stdout.write("Deleted %i descriptors" % (deleted_descriptors))
+		deleted_descriptors = Descriptor.objects.filter(created__lt=cutoff).delete()
+		self.stdout.write("Deleted %i descriptors" % (deleted_descriptors[0]))
