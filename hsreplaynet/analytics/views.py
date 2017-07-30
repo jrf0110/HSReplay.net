860eadd4ff1ff52d85a918b355a8b7e0fd071029
Benedict Etzel <benedict@hearthsim.net>
2017-07-30 18:40:00 +0200

    Revert "Change analytics response for non-eligible decks to 204"
    This reverts commit 06cf601e05c6e5039ff5d64897f18233f27289cc.

--- hsreplaynet/analytics/views.py
+++ hsreplaynet/analytics/views.py
@@ -124,7 +124,7 @@ def _get_query_and_params(request, name):
 			return HttpResponseForbidden()
 	else:
 		if deck and not deck_is_eligible_for_global_stats(deck):
-			return HttpResponse(status=204)
+			return HttpResponseForbidden()
 
 		try:
 			parameterized_query = query.build_full_params(supplied_params)
