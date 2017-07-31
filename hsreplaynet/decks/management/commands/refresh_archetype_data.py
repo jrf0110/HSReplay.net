65cc05bfe673cfce343b879f5b3644639afbba51
Andrew Wilson <andrewmoorewilson@gmail.com>
2017-07-31 15:12:32 -0400

    Add refresh archetype data management command

---
+++ hsreplaynet/decks/management/commands/refresh_archetype_data.py
@@ -0,0 +1,20 @@
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from hsreplaynet.analytics.processing import write_messages_to_queue
+from hsreplaynet.utils.aws.redshift import get_redshift_catalogue
+
+
+class Command(BaseCommand):
+	def handle(self, *args, **options):
+		catalogue = get_redshift_catalogue()
+		queue_name = settings.REDSHIFT_ANALYTICS_QUERY_QUEUE_NAME
+		queries = []
+		for name, query in catalogue.registry.items():
+			if query.uses_archetypes:
+				query.mark_all_stale()
+				for permutation in query.generate_cachable_parameter_permutations():
+					queries.append({
+						"query_name": query.name,
+						"supplied_parameters": permutation
+					})
+		write_messages_to_queue(queue_name, queries)
