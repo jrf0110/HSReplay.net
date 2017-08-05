23c0be9a22749ba48d381e6339dbb7b6cea078d9
Alexander Zeier <alex@hearthsim.net>
2017-08-05 22:31:03 +0200

    Require trailing slash on live data urls

--- hsreplaynet/live/urls.py
+++ hsreplaynet/live/urls.py
@@ -4,12 +4,12 @@ from . import views
 
 urlpatterns = [
 	url(
-		r"^distributions/player_class/(?P<game_type_name>\w+)$",
+		r"^distributions/player_class/(?P<game_type_name>\w+)/$",
 		views.fetch_player_class_distribution,
 		name="live_fetch_player_class_distribution"
 	),
 	url(
-		r"^distributions/played_cards/(?P<game_type_name>\w+)$",
+		r"^distributions/played_cards/(?P<game_type_name>\w+)/$",
 		views.fetch_played_cards_distribution,
 		name="live_fetch_played_cards_distribution"
 	),
