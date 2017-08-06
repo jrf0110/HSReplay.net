ee1bd410d71c78bd0f3f53e9c6b65e9c98533422
Benedict Etzel <benedict@hearthsim.net>
2017-08-06 15:37:42 +0200

    Fix test_load_homepage using pytest-mock
    Mock HomeView.get_winrate_data until we start mocking
    aws.utils.redshift.

--- tests/test_web.py
+++ tests/test_web.py
@@ -9,8 +9,13 @@ def test_battlenet_auth_loaded():
 
 
 @pytest.mark.django_db
-def test_load_homepage(client, settings):
+def test_load_homepage(client, settings, mocker):
 	# Disable (Manifest)StaticFilesStorage, since tests will not run collectstatic
 	settings.STATICFILES_STORAGE = settings.DEFAULT_FILE_STORAGE
+
+	# Until we start mocking aws.utils.redshift, mock HomeView.get_winrate_data directly
+	with mocker.patch("hsreplaynet.views.HomeView.get_winrate_data") as get_winrate_data:
+		get_winrate_data.return_value = {"standard": {}, "wild": {}}
+
 	response = client.get("/")
 	assert response.status_code == 200
