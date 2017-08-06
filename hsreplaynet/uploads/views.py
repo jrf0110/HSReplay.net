71a11e0d92a39bce3431621ae632e0c043cd8ea1
Jerome Leclanche <jerome@leclan.ch>
2017-08-07 02:28:59 +0300

    Allow disabling UploadEvent lookups in uploads view
    Closes #397

--- hsreplaynet/uploads/views.py
+++ hsreplaynet/uploads/views.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views.generic import View
@@ -11,13 +12,18 @@ class UploadDetailView(View):
 		if replay:
 			return HttpResponseRedirect(replay.get_absolute_url())
 
-		try:
-			upload = UploadEvent.objects.get(shortid=shortid)
-			if upload.game:
-				return HttpResponseRedirect(upload.game.get_absolute_url())
-		except UploadEvent.DoesNotExist:
-			# It is possible the UploadEvent hasn't been created yet.
+		# This setting lets us prevent the only site-wide queyr on UploadEvent.
+		# Bit of a hack but it does the job for now.
+		if getattr(settings, "UPLOADS_DB_DISABLED", False):
 			upload = None
+		else:
+			try:
+				upload = UploadEvent.objects.get(shortid=shortid)
+				if upload.game:
+					return HttpResponseRedirect(upload.game.get_absolute_url())
+			except UploadEvent.DoesNotExist:
+				# It is possible the UploadEvent hasn't been created yet.
+				upload = None
 
 		request.head.title = "Uploading replay..."
 
