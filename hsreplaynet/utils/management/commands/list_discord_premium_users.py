6509e7c0943c91d6ef657a47880dff8117ba4dc1
Jerome Leclanche <jerome@leclan.ch>
2017-08-08 13:35:32 +0300

    Implement a management command to list Discord premium users

---
+++ hsreplaynet/utils/management/commands/list_discord_premium_users.py
@@ -0,0 +1,19 @@
+import json
+from allauth.socialaccount.models import SocialAccount
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+	def handle(self, *args, **options):
+		ret = []
+
+		for acc in SocialAccount.objects.filter(provider="discord"):
+			if acc.user.is_premium:
+				ret.append({
+					"user_id": acc.user_id,
+					"discord_username": acc.extra_data.get("username", ""),
+					"discord_discriminator": acc.extra_data.get("discriminator", ""),
+					"discord_id": acc.extra_data.get("id", ""),
+				})
+
+		self.stdout.write(json.dumps(ret))
