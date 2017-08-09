8131e45ae502f98144dccdb048238562b3ec0549
Benedict Etzel <benedict@hearthsim.net>
2017-08-09 15:15:13 +0200

    Deprecate PrettyTime.getAge

--- hsreplaynet/static/scripts/src/PrettyTime.ts
+++ hsreplaynet/static/scripts/src/PrettyTime.ts
@@ -4,6 +4,9 @@ export function getDuration(from: Date, to: Date): string {
 	return moment(from).from(moment(to), true);
 }
 
+/**
+ * @deprecated Use SemanticAge component instead
+ */
 export function getAge(since: Date, noSuffix?: boolean): string {
 	return moment(since).utc().from(new Date(), noSuffix);
 }
