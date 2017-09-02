ac672a32efb1258c7a1cd7c90aff28e77b42a703
Andrew Wilson <andrewmoorewilson@gmail.com>
2017-09-02 16:02:26 +0200

    Add more memory to Redshift ETL

--- hsreplaynet/lambdas/crons.py
+++ hsreplaynet/lambdas/crons.py
@@ -10,7 +10,7 @@ from hsreplaynet.utils.instrumentation import lambda_handler
 	cpu_seconds=300,
 	requires_vpc_access=True,
 	tracing=False,
-	memory=256,
+	memory=320,
 )
 def do_redshift_etl_maintenance(event, context):
 	"""A periodic job to orchestrate Redshift ETL Maintenance"""
