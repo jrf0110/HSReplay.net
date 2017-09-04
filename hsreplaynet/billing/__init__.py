0b569689062ed0ce81247c1f3b8313daa6eaea53
Jerome Leclanche <jerome@leclan.ch>
2017-09-05 02:39:59 +0300

    Remove unused webhook handler

--- hsreplaynet/billing/__init__.py
+++ hsreplaynet/billing/__init__.py
@@ -1,30 +1,4 @@
 from djstripe import webhooks
-from hsreplaynet.utils import log
-from hsreplaynet.utils.influx import influx_metric
-
-
-# @webhooks.handler("customer.subscription.created")
-def customer_subscription_created_handler(event):
-	from hsreplaynet.analytics.processing import (
-		PremiumUserCacheWarmingContext, warm_redshift_cache_for_user_context
-	)
-	if event.customer and event.customer.subscriber:
-		user = event.customer.subscriber
-		log.info("Received premium purchased signal for user: %s" % user.username)
-		log.info("Scheduling personalized stats for immediate cache warming.")
-		context = PremiumUserCacheWarmingContext.from_user(user)
-
-		fields = {
-			"count": 1,
-			"user": user.username
-		}
-
-		influx_metric(
-			"premium_purchase_cache_warm_event",
-			fields,
-		)
-
-		warm_redshift_cache_for_user_context(context)
 
 
 @webhooks.handler("customer.subscription.created")
