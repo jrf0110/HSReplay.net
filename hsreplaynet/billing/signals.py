fcd5a4508d9d1636b0ee14e0e47941dcbaba5bb5
Jerome Leclanche <jerome@leclan.ch>
2017-09-05 03:36:21 +0300

    Add Paypal webhook handling

--- hsreplaynet/billing/signals.py
+++ hsreplaynet/billing/signals.py
@@ -1,9 +1,17 @@
+from djpaypal.models.webhooks import webhook_handler as djpaypal_webhook_handler
 from djstripe.webhooks import handler as djstripe_webhook_handler
 from hsreplaynet.analytics.processing import enable_premium_accounts_for_users_in_redshift
 
 
 @djstripe_webhook_handler("customer.subscription.created")
-def sync_premium_accounts_for_subscription(event):
+def sync_premium_accounts_for_stripe_subscription(event):
 	if event.customer and event.customer.subscriber:
 		user = event.customer.subscriber
 		enable_premium_accounts_for_users_in_redshift([user])
+
+
+@djpaypal_webhook_handler("billing.subscription.*")
+def sync_premium_accounts_for_paypal_subscription(sender, event):
+	subscription = event.get_resource()
+	if subscription.user and subscription.user.is_premium:
+		enable_premium_accounts_for_users_in_redshift([subscription.user])
