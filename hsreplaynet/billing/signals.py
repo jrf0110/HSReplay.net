a4ba033d95405c997496bb2c42f9eb6cd6e34b62
Jerome Leclanche <jerome@leclan.ch>
2017-09-05 16:34:18 +0300

    Accept **kwargs in billing.signals

--- hsreplaynet/billing/signals.py
+++ hsreplaynet/billing/signals.py
@@ -4,14 +4,14 @@ from hsreplaynet.analytics.processing import enable_premium_accounts_for_users_i
 
 
 @djstripe_webhook_handler("customer.subscription.created")
-def sync_premium_accounts_for_stripe_subscription(event):
+def sync_premium_accounts_for_stripe_subscription(event, **kwargs):
 	if event.customer and event.customer.subscriber:
 		user = event.customer.subscriber
 		enable_premium_accounts_for_users_in_redshift([user])
 
 
 @djpaypal_webhook_handler("billing.subscription.*")
-def sync_premium_accounts_for_paypal_subscription(sender, event):
+def sync_premium_accounts_for_paypal_subscription(sender, event, **kwargs):
 	subscription = event.get_resource()
 	if subscription.user and subscription.user.is_premium:
 		enable_premium_accounts_for_users_in_redshift([subscription.user])
