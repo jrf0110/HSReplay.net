8b7cd643a84e4ef84f9aa3f07f3dccbcc65b9050
Benedict Etzel <benedict@hearthsim.net>
2017-09-06 14:16:44 +0200

    Add Paypal announcement tooltip behind featuretag
    Closes #465.

--- hsreplaynet/static/scripts/src/entries/polyfills.ts
+++ hsreplaynet/static/scripts/src/entries/polyfills.ts
@@ -1,6 +1,48 @@
+import * as $ from "jquery";
+import {cookie} from "cookie_js";
+import UserData from "../UserData";
+
 if (window && window.navigator && window.navigator.userAgent && /Edge\/1[0-4]\./.test(window.navigator.userAgent)) {
 	// Fix for bug in Microsoft Edge: https://github.com/Microsoft/ChakraCore/issues/1415#issuecomment-246424339
 	Function.prototype.call = function (t) {
 		return this.apply(t, Array.prototype.slice.apply(arguments, [1]));
 	};
+}
+
+if (document.location.pathname.match(/\/(replay|games|articles|decks|cards|meta|discover)\//)) {
+	UserData.create();
+	document.addEventListener("DOMContentLoaded", () => {
+		// locate the premium navbar item
+		const premium = document.getElementById("navbar-link-premium");
+		if (!premium) {
+			return;
+		}
+
+		// do not show if feature is disabled
+		if (!UserData.hasFeature("paypal-announcement-tooltip")) {
+			return;
+		}
+
+		// do not show if premium user or hidden
+		if (UserData.isPremium() || cookie.get("knows-about-paypal", "0") !== "0") {
+			return;
+		}
+
+		($(premium) as any).popover({
+			animation: true,
+			trigger: "manual",
+			placement: "bottom",
+			html: true,
+			title: "Highly requested <a href=\"#\" id=\"premium-popover-close\" aria-hidden=\"true\">&times;</a>",
+			content: "We accept PayPal now!",
+		});
+		($(premium) as any).on("shown.bs.popover", () => {
+			$("#premium-popover-close").click((evt) => {
+				evt.preventDefault();
+				($(premium) as any).popover("destroy");
+				cookie.set("knows-about-paypal", "1", {path: "/", expires: 365});
+			})
+		});
+		setTimeout(() => ($(premium) as any).popover("show"), 500);
+	});
 }
