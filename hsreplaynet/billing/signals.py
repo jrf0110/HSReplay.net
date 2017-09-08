b51342f6f720f85ea93424cebbf650f450635c8e
Jerome Leclanche <jerome@leclan.ch>
2017-09-08 10:38:17 +0300

    Send Paypal webhook exceptions to Sentry
    We can't easily send the original traceback, so until we add Sentry
    support to dj-paypal we'll just raise a custom error.

--- hsreplaynet/billing/signals.py
+++ hsreplaynet/billing/signals.py
@@ -1,6 +1,11 @@
-from djpaypal.models.webhooks import webhook_handler as djpaypal_webhook_handler
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from djpaypal.models.webhooks import (
+	webhook_handler as djpaypal_webhook_handler, WebhookEventTrigger
+)
 from djstripe.webhooks import handler as djstripe_webhook_handler
 from hsreplaynet.analytics.processing import enable_premium_accounts_for_users_in_redshift
+from hsreplaynet.utils.instrumentation import error_handler
 
 
 @djstripe_webhook_handler("customer.subscription.created")
@@ -15,3 +20,12 @@ def sync_premium_accounts_for_paypal_subscription(sender, event, **kwargs):
 	subscription = event.get_resource()
 	if subscription.user and subscription.user.is_premium:
 		enable_premium_accounts_for_users_in_redshift([subscription.user])
+
+
+@receiver(post_save, sender=WebhookEventTrigger)
+def on_paypal_webhook_error(sender, instance, **kwargs):
+	if instance.exception:
+		try:
+			raise Exception("%s - %s" % (instance.exception, instance.id))
+		except Exception:
+			error_handler()
