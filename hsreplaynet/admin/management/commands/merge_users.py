60e8b38071bd39a767198f891988d0bb2f2da76c
Jerome Leclanche <jerome@leclan.ch>
2017-09-15 21:19:09 +0300

    Move merge_users command and functionality to hsreplaynet.admin

--- hsreplaynet/admin/management/commands/merge_users.py
+++ hsreplaynet/admin/management/commands/merge_users.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import get_user_model
+from django.core.management.base import BaseCommand, CommandError
 from djstripe.models import Customer
 from hsreplaynet.utils import log
 
@@ -58,3 +60,28 @@ def merge_users(base_user, user):
 		log.info("Found Stripe customer on %r, nothing to do.", base_user)
 	else:
 		log.info("No Stripe customers to merge.")
+
+
+class Command(BaseCommand):
+	def add_arguments(self, parser):
+		parser.add_argument("base_user", nargs=1)
+		parser.add_argument("user", nargs=1)
+
+	def handle(self, *args, **options):
+		base_username = options["base_user"]
+		username = options["user"]
+		User = get_user_model()
+
+		try:
+			base_user = User.objects.get(username=base_username[0])
+			user = User.objects.get(username=username[0])
+		except User.DoesNotExist as e:
+			raise CommandError(e)
+
+		if input("Merge user %r into %r? [y/N] " % (user, base_user)).lower() != "y":
+			raise CommandError("Not merging users.")
+			return
+
+		merge_users(base_user, user)
+
+		self.stdout.write("Users have been merged.")
