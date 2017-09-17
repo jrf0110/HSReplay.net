982b39449ce37604744ec200eadd31c59b580267
Jerome Leclanche <jerome@leclan.ch>
2017-09-17 19:40:39 +0300

    Fix bad escape in article URLs

--- hsreplaynet/articles/urls.py
+++ hsreplaynet/articles/urls.py
@@ -9,7 +9,7 @@ list_view = views.ArticleListView.as_view()
 
 
 urlpatterns = [
-	url("^$", list_view, name="articles_article_list"),
-	url("^latest.atom$", LatestArticlesFeed(), name="articles_article_feed"),
-	url("^(?P<pk>\d+)/(?P<slug>[\w-]+)?", detail_view, name="articles_article_detail"),
+	url(r"^$", list_view, name="articles_article_list"),
+	url(r"^latest.atom$", LatestArticlesFeed(), name="articles_article_feed"),
+	url(r"^(?P<pk>\d+)/(?P<slug>[\w-]+)?", detail_view, name="articles_article_detail"),
 ]
