8ef27c026d777e81418786d01986a60fbbe6d084
Alexander Zeier <alex@hearthsim.net>
2017-09-20 12:03:05 +0200

    Add generic InfoboxItem component

---
+++ hsreplaynet/static/scripts/src/components/InfoboxItem.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+
+interface InfoboxItemProps extends React.ClassAttributes<InfoboxItem> {
+	header: string;
+	value?: any;
+}
+
+export default class InfoboxItem extends React.Component<InfoboxItemProps, {}> {
+	render(): JSX.Element {
+		return (
+			<li>
+				{this.props.header}
+				<span className="infobox-value">{this.props.value}</span>
+			</li>
+		);
+	}
+}
