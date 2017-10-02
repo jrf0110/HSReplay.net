1219ec1a2a7df349da1d37c9c2f373b3d1f19129
Andrew Wilson <andrewmoorewilson@gmail.com>
2017-10-02 10:51:23 -0400

    Update management command to mark archetypes stale

--- hsreplaynet/decks/management/commands/refresh_archetype_data.py
+++ hsreplaynet/decks/management/commands/refresh_archetype_data.py
@@ -1,22 +1,16 @@
 from django.conf import settings
 from django.core.management.base import BaseCommand
 
-from hsreplaynet.analytics.processing import write_messages_to_queue
 from hsreplaynet.utils.aws.redshift import get_redshift_catalogue
 
 
 class Command(BaseCommand):
 	def handle(self, *args, **options):
 		catalogue = get_redshift_catalogue()
-		queue_name = settings.REDSHIFT_ANALYTICS_QUERY_QUEUE_NAME
-		queries = []
 		for name, query in catalogue.registry.items():
 			if query.uses_archetypes:
 				query.mark_all_stale()
 				if name in settings.ARCHETYPE_QUERIES_FOR_IMMEDIATE_REFRESH:
 					for permutation in query.generate_cachable_parameter_permutations():
-						queries.append({
-							"query_name": query.name,
-							"supplied_parameters": permutation
-						})
-		write_messages_to_queue(queue_name, queries)
+						parameterized_query = query.build_full_params(permutation)
+						parameterized_query.schedule_refresh()
