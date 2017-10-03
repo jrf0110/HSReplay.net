fb70422669ad13a11bd7bcd474075f64cce7bf95
Andrew Wilson <andrewmoorewilson@gmail.com>
2017-10-03 14:41:09 -0400

    Fix usage of dateutil parser

--- hsreplaynet/uploads/management/commands/reprocess_upload_events.py
+++ hsreplaynet/uploads/management/commands/reprocess_upload_events.py
@@ -1,4 +1,4 @@
-from dateutil import parse as parse_timestamp
+from dateutil.parser import parse as parse_timestamp
 from django.core.management.base import BaseCommand
 
 from hsreplaynet.uploads.processing import queue_upload_events_for_reprocessing
