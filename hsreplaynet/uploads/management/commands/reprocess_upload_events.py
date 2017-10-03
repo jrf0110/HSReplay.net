2307ac9520f99cb3f588377d17c32f9bef147242
Andrew Wilson <andrewmoorewilson@gmail.com>
2017-10-03 14:08:11 -0400

    Fix reprocess old upload events

--- hsreplaynet/uploads/management/commands/reprocess_upload_events.py
+++ hsreplaynet/uploads/management/commands/reprocess_upload_events.py
@@ -1,7 +1,8 @@
 from dateutil import parse as parse_timestamp
 from django.core.management.base import BaseCommand
 
-from hsreplaynet.games.processing import do_process_upload_event
+from hsreplaynet.uploads.processing import queue_upload_events_for_reprocessing
+
 from hsreplaynet.uploads.models import UploadEvent
 
 
@@ -15,10 +16,6 @@ class Command(BaseCommand):
 			"--to", required=True,
 			help="End date (YYYY-MM-DD HH:MM:SSZ)",
 		)
-		parser.add_argument(
-			"--shortid", nargs="*",
-			help="Limit reprocessing to specific shortids"
-		)
 
 	def handle(self, *args, **options):
 		from_date = parse_timestamp(options["from"])
@@ -28,10 +25,4 @@ class Command(BaseCommand):
 			created__lte=to_date
 		)
 
-		shortid = options.get("shortid")
-		if shortid:
-			uploads = uploads.filter(shortid__in=shortid)
-
-		for upload in uploads:
-			print("Reprocessing %r" % (upload))
-			do_process_upload_event(upload)
+		queue_upload_events_for_reprocessing(uploads, use_kinesis=True)
