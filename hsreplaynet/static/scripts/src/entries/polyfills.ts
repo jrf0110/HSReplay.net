bae61a974826a27a8c4a9bb60034c8d409bafd3e
Jerome Leclanche <jerome@leclan.ch>
2017-11-11 01:26:58 -0500

    Drop Paypal announcement tooltip code

--- hsreplaynet/static/scripts/src/entries/polyfills.ts
+++ hsreplaynet/static/scripts/src/entries/polyfills.ts
@@ -9,51 +9,6 @@ if (window && window.navigator && window.navigator.userAgent && /Edge\/1[0-4]\./
 	};
 }
 
-if (document.location.pathname.match(/\/(replay|games|articles|decks|cards|meta|discover)\//)) {
-	UserData.create();
-	document.addEventListener("DOMContentLoaded", () => {
-		// locate the premium navbar item
-		const premium = document.getElementById("navbar-link-premium");
-		if (!premium) {
-			return;
-		}
-
-		// do not show if feature is disabled
-		if (!UserData.hasFeature("paypal-announcement-tooltip")) {
-			return;
-		}
-
-		// do not show if premium user or hidden
-		if (UserData.isPremium() || cookie.get("knows-about-paypal", "0") !== "0") {
-			return;
-		}
-
-		// do not show to ineligible countries
-		const country = UserData.getIpCountry();
-		if (country && ["DE", "CN"].indexOf(country.toUpperCase()) !== -1) {
-			return;
-		}
-
-		($(premium) as any).popover({
-			animation: true,
-			trigger: "manual",
-			placement: "bottom",
-			html: true,
-			title: "Highly requested <a href=\"#\" id=\"premium-popover-close\" class=\"popover-close\" aria-hidden=\"true\">&times;</a>",
-			content: "We accept PayPal now!",
-			template: "<div class=\"popover premium-popover\"><div class=\"arrow\"></div><h3 class=\"popover-title\"></h3><div class=\"popover-content\"></div></div>",
-		});
-		($(premium) as any).on("shown.bs.popover", () => {
-			$("#premium-popover-close").click((evt) => {
-				evt.preventDefault();
-				($(premium) as any).popover("destroy");
-				cookie.set("knows-about-paypal", "1", {path: "/", expires: 365});
-			});
-		});
-		setTimeout(() => ($(premium) as any).popover("show"), 500);
-	});
-}
-
 if (document.location.pathname.match(/\/(replay|games|articles|decks|cards)\//)) {
 	UserData.create();
 	document.addEventListener("DOMContentLoaded", () => {
