a274cb8c7b89d7a91846080bcdb07269b4427d52
Jerome Leclanche <jerome@leclan.ch>
2017-11-12 22:27:19 -0500

    Prevent promotion of a cluster snapshot if live archetypes are lost

--- hsreplaynet/decks/management/commands/update_archetype_signatures.py
+++ hsreplaynet/decks/management/commands/update_archetype_signatures.py
@@ -1,6 +1,14 @@
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
 
 from hsreplaynet.decks.models import ClusterSetSnapshot
+
+
+def get_archetype_ids(cluster_set):
+	for class_cluster in cluster_set.class_clusters:
+		for cluster in class_cluster.clusters:
+			if cluster.external_id == -1 or not cluster.external_id:
+				continue
+			yield cluster.external_id
 
 
 class Command(BaseCommand):
@@ -9,6 +17,14 @@ class Command(BaseCommand):
 
 	def handle(self, *args, **options):
 		force = bool(options["force"])
+		current_set = ClusterSetSnapshot.objects.filter(live_in_production=True).first()
 		cluster_set = ClusterSetSnapshot.objects.filter(latest=True).first()
 		if cluster_set:
+			if current_set:
+				archetypes_in_production = get_archetype_ids(current_set)
+				archetypes_to_go = get_archetype_ids(cluster_set)
+				remainder = set(archetypes_in_production) - set(archetypes_to_go)
+				if remainder:
+					raise CommandError("Archetypes missing: %r. Will not promote.")
+
 			cluster_set.update_archetype_signatures(force=force)
