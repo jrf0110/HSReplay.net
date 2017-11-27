fb4d9be72be5da63d230af9b256e753cce8cbe7f
Jerome Leclanche <jerome@leclan.ch>
2017-11-27 07:02:10 +0200

    Make admin use allauth login

--- hsreplaynet/admin/urls.py
+++ hsreplaynet/admin/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth.decorators import login_required
 
+
+admin.site.login = login_required(admin.site.login)
 
 urlpatterns = [
 	url(r"^", admin.site.urls),
