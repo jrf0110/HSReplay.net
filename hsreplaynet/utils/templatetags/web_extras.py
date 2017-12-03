af53ba58d0e39b033fc7147297a2df46dc603279
Jerome Leclanche <jerome@leclan.ch>
2017-12-03 03:24:42 +0200

    Remove a deprecated mark_for_escaping call

--- hsreplaynet/utils/templatetags/web_extras.py
+++ hsreplaynet/utils/templatetags/web_extras.py
@@ -5,7 +5,7 @@ from django import template
 from django.conf import settings
 from django.contrib.staticfiles.templatetags.staticfiles import static
 from django.urls import reverse
-from django.utils.safestring import mark_for_escaping, mark_safe
+from django.utils.safestring import mark_safe
 
 
 register = template.Library()
@@ -75,7 +75,7 @@ def static_absolute(context, value):
 def nav_active(context, name, css="active"):
 	request = context.request
 	if request.path == reverse(name):
-		return mark_safe(' class="%s"' % (mark_for_escaping(css)))
+		return mark_safe(f' class="{css}"')
 	return ""
 
 
