68cd1c27edac84eb63cebfc0959095d5b1a87267
Benedict Etzel <benedict@hearthsim.net>
2017-12-06 15:43:23 +0100

    Improve readability of PopularityMatrix

--- hsreplaynet/static/scripts/src/components/metaoverview/popularity/ColumnFooter.tsx
+++ hsreplaynet/static/scripts/src/components/metaoverview/popularity/ColumnFooter.tsx
@@ -10,12 +10,13 @@ interface ColumnFooterProps extends React.ClassAttributes<ColumnFooter> {
 export default class ColumnFooter extends React.Component<ColumnFooterProps, {}> {
 	render() {
 		const lightness = 45 + Math.floor(55 * Math.max(0, (1 - (this.props.games || 0 ) / (this.props.maxGames))));
+		const color = lightness > 60 ? "black" : "white";
 		const backgroundColor = `hsl(214,50%,${lightness}%)`;
 
 		return (
 			<div
 				className="matchup-column-footer matchup-column-footer-games popularity-column-footer"
-				style={{backgroundColor, ...this.props.style}}
+				style={{color, backgroundColor, ...this.props.style}}
 			>
 				{commaSeparate(this.props.games)}
 			</div>
--- hsreplaynet/static/scripts/src/components/metaoverview/popularity/PopularityCell.tsx
+++ hsreplaynet/static/scripts/src/components/metaoverview/popularity/PopularityCell.tsx
@@ -11,12 +11,13 @@ export default class PopularityCell extends React.Component<PopularityCellProps,
 	render() {
 		const classNames = ["matchup-cell"];
 		const lightness = 45 + Math.floor(55 * Math.max(0, (1 - (this.props.popularity || 0 ) / (this.props.maxPopularity))));
+		const color = lightness > 60 ? "black" : "white";
 		const backgroundColor = `hsl(214,50%,${lightness}%)`;
 
 		return (
 			<div
 				className={classNames.join(" ")}
-				style={{backgroundColor, ...this.props.style}}
+				style={{color, backgroundColor, ...this.props.style}}
 			>
 				{this.props.popularity ? toDynamicFixed(this.props.popularity, 2) : 0}%
 			</div>
