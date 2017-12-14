269527d4100f98a9f33ee52b9ad31c9bdd82716c
Benedict Etzel <benedict@hearthsim.net>
2017-12-14 23:24:21 +0100

    Clean up Tab.tsx

--- hsreplaynet/static/scripts/src/components/layout/Tab.tsx
+++ hsreplaynet/static/scripts/src/components/layout/Tab.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-interface TabProps {
+interface TabProps extends React.ClassAttributes<Tab> {
 	id: string;
 	hidden?: boolean;
 	disabled?: boolean;
@@ -8,7 +8,7 @@ interface TabProps {
 	highlight?: boolean;
 }
 
-export default class Tab extends React.Component<TabProps, {}> {
+export default class Tab extends React.Component<TabProps> {
 	render() {
 		return <div>{this.props.children}</div>;
 	}
