6c3180612aa202b98c9be60589fa6750f095e8c6
Jerome Leclanche <jerome@leclan.ch>
2017-12-14 20:36:48 +0200

    Add Billing models administrations

---
+++ hsreplaynet/billing/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+
+from . import models
+
+
+@admin.register(models.LazyDiscount)
+class LazyDiscountAdmin(admin.ModelAdmin):
+	list_display = ("__str__", "user", "coupon", "used", "created", "updated")
+	list_filter = ("used", )
+	raw_id_fields = ("user", "referral_source")
+
+
+@admin.register(models.Referral)
+class ReferralAdmin(admin.ModelAdmin):
+	list_display = (
+		"__str__", "credited_amount", "processed", "credited_user", "hit_user", "created"
+	)
+	list_filter = ("processed", )
+	raw_id_fields = ("referral_hit", "credited_user", "hit_user")
+
+
+@admin.register(models.ReferralLinkPlan)
+class ReferralLinkPlanAdmin(admin.ModelAdmin):
+	list_display = ("__str__", "referral_link", "apply_coupon", "coupon_expiry")
+	raw_id_fields = ("referral_link", )
